e8ba30cbe7e3254bd97b281dfbdf65decd177182
Óscar Nájera <najera.oscar@gmail.com>
2014-11-11 12:34:27 +0100

    Completely local selfenergy

--- triqs/ipt_AFM.py
+++ triqs/ipt_AFM.py
@@ -12,6 +12,7 @@ from pytriqs.plot.mpl_interface import oplot
 from pytriqs.plot.mpl_interface import subplots
 import matplotlib.pyplot as plt
 
+
 class IPTSolver(object):
 
     def __init__(self, **params):
@@ -24,34 +25,28 @@ class IPTSolver(object):
         self.gdw = self.gup.copy()
         self.g0up = self.gup.copy()
         self.g0dw = self.gup.copy()
-        self.sigmaup = self.gup.copy()
-        self.sigmadw = self.gup.copy()
+        self.sigma = self.gup.copy()
 
         # Imaginary time
-        self.g0tup = GfImTime(indices=[0], beta=self.beta)
-        self.g0tdw = self.g0tup.copy()
-        self.sigmatup = self.g0tup.copy()
-        self.sigmatdw = self.g0tup.copy()
+        self.g0t = GfImTime(indices=[0], beta=self.beta)
+        self.sigmat = self.g0t.copy()
 
     def solve(self):
 
-        self.g0tup <<= InverseFourier(self.g0up)
-        self.sigmatup <<= (self.U**2) * self.g0tup * self.g0tdw * self.g0tdw
-        self.sigmaup <<= Fourier(self.sigmatup)
-
-        self.g0tdw <<= InverseFourier(self.g0dw)
-        self.sigmatdw <<= (self.U**2) * self.g0tdw * self.g0tup * self.g0tup
-        self.sigmadw <<= Fourier(self.sigmatdw)
+        self.g0t <<= InverseFourier(self.g0up)
+        self.sigmat <<= (self.U**2) * self.g0t * self.g0t * self.g0t
+        self.sigma <<= Fourier(self.sigmat)
 
         # Dyson equation to get G
-        self.gup <<= self.g0up * inverse(1.0 - self.sigmaup * self.g0up)
-        self.gdw <<= self.g0dw * inverse(1.0 - self.sigmadw * self.g0dw)
+        self.gup <<= self.g0up * inverse(1.0 - self.sigma * self.g0up)
+        self.gdw <<= self.g0dw * inverse(1.0 - self.sigma * self.g0dw)
 
     def dmft_loop(self, n_loops, t):
         for i in range(n_loops):
             self.g0up <<= inverse(iOmega_n - t**2 * self.gdw)
             self.g0dw <<= inverse(iOmega_n - t**2 * self.gup)
             self.solve()
+
 
 def ev_on_loops(n_loops, U, beta, t):
     """Studies change of Green's function depending on DMFT loop count"""
@@ -61,26 +56,26 @@ def ev_on_loops(n_loops, U, beta, t):
 
     for loop in n_loops:
         S = IPTSolver(U=U, beta=beta)
-        S.gup <<= SemiCircular(2*t) + inverse( iOmega_n + 0.5 )
+        S.gup <<= inverse(iOmega_n +0.1+ 0.5j)
 
-        S.gdw <<= SemiCircular(2*t) + inverse( iOmega_n + 0.4 )
+        S.gdw <<= inverse(iOmega_n + 0.4j)
 
         S.dmft_loop(loop, t)
-        ax1.oplot(S.sigmaup, '-o', RI='I', x_window  = (0,2), label = "$\uparrow$ nloop={}".format(loop))
-        ax1.oplot(S.sigmadw, '-+', RI='I', x_window  = (0,2), label = "$\downarrow$ nloop={}".format(loop))
+        ax1.oplot(S.sigma, '-o', RI='I', x_window=(0, 2),
+                  label="nloop={}".format(loop))
         ax1.set_ylabel('$\Sigma(i\omega_n)$')
 
-        ax2.oplot(S.g0tup, label = "$\uparrow$ nloop={}".format(loop))
-        ax2.oplot(S.g0tdw,'--', label = "$\downarrow$ nloop={}".format(loop))
+        ax2.oplot(S.g0t, label="nloop={}".format(loop))
 
-        grealup = GfReFreq(indices=[1], window=(-4,4), n_points=400)
+        grealup = GfReFreq(indices=[1], window=(-4, 4), n_points=400)
         grealdw = grealup.copy()
-        grealup.set_from_pade(S.gup,201,0.0)
-        grealdw.set_from_pade(S.gdw,201,0.0)
-        ax3.oplot(grealup, RI='I', label = "$\uparrow$  nloop={}".format(loop))
-        ax3.oplot(grealdw,'--', RI='I', label = "$\downarrow$ nloop={}".format(loop))
+        grealup.set_from_pade(S.gup, 201, 0.0)
+        grealdw.set_from_pade(S.gdw, 201, 0.0)
+        ax3.oplot(grealup, RI='I', label="$\uparrow$  nloop={}".format(loop))
+        ax3.oplot(grealdw, '--', RI='I',
+                  label="$\downarrow$ nloop={}".format(loop))
 
-    ax1.set_title("Matsubara Green's functions, IPT, Bethe lattice, $\\beta=%.2f$, $U=%.2f$"%(beta,U))
+    ax1.set_title("Matsubara Green's functions, IPT, Bethe lattice, $\\beta=%.2f$, $U=%.2f$" % (beta, U))
 
 
 def ev_on_interaction(n_loops, U, beta, t):
@@ -96,7 +91,7 @@ def ev_on_interaction(n_loops, U, beta, t):
     plt.title("Matsubara Green's functions, IPT, Bethe lattice, $\\beta=%.2f$, $loop=%.2f$".format(beta,n_loops))
 
 if __name__ == "__main__":
-    ev_on_loops([15], 1.5, 50, 0.5)
+    ev_on_loops([15], 2., 55, 0.5)
 
 
             # Get the real-axis with Pade approximants
