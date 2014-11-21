9389d4d5b728eb1c9d6e01147ac7d6533f1adcfc
Óscar Nájera <najera.oscar@gmail.com>
2014-11-21 13:52:14 +0100

    fine imaginary widening, don't calculate Lat G in solve

--- dmft/twosite.py
+++ dmft/twosite.py
@@ -30,55 +30,67 @@ class twosite(object):
 
     def __init__(self, beta, t, freq_axis):
         """Sets up environment
-           Parameters
-           ----------
-           beta : float
-                  Inverse temperature of the system
-           t : float
-               Hopping amplitude between first neighbor lattice sites
-           freq_axis: string
-                      'real' or 'matsubara' frequencies
 
-            Attributes
-            ----------
-            GF : dictionary
-                 Stores the Green functions and self enerry"""
+        Parameters
+        ----------
+        beta : float
+               Inverse temperature of the system
+        t : float
+            Hopping amplitude between first neighbor lattice sites
+        freq_axis: string
+                   'real' or 'matsubara' frequencies
+
+         Attributes
+         ----------
+         GF : dictionary
+              Stores the Green functions and self enerry
+        """
         self.beta = beta
         self.t = t
         self.m2 = m2_weight(t, 200)
         self.freq_axis = freq_axis
 
+        self.x = np.linspace(-4, 4, 800)
         if freq_axis == 'real':
-            self.omega = np.linspace(-4, 4, 3500) + 8e-2j
+            self.omega = self.x + 1e-6j
         elif freq_axis == 'matsubara':
-            self.omega = 1j*np.arange(1, 3500, 2) / self.beta
+            self.omega = 1j*np.arange(1, 400, 2) / self.beta
         else:
             raise ValueError('Set a working frequency axis')
 
+        self.rho_0 = dos.bethe_lattice(self.x, self.t)
         self.eig_energies = None
         self.eig_states = None
         self.oper = [fermion.destruct(4, index) for index in range(4)]
+        self.H_operators = self.hamiltonian()
         self.GF = {}
 
-    def hamiltonian(self, mu, e_c, u_int, hyb):
+    def hamiltonian(self):
         """Two site single impurity anderson model
+        generate the matrix operators that will be used for this hamiltonian
 
-           .. math:
-              \mathcal{H} = -\mu d^\dagger_\sigma d_sigma
-              + (\epsilon - \mu) c^\dagger_\sigma c_\sigma +
-              U d^\dagger_\\uparrow d_\\uparrow d^\dagger_\downarrow d_\downarrow
-              + V(d^\dagger_\sigma c_\sigma + h.c.)"""
+        .. math:
+           \mathcal{H} = -\mu d^\dagger_\sigma d_sigma
+           + (\epsilon - \mu) c^\dagger_\sigma c_\sigma +
+           U d^\dagger_\\uparrow d_\\uparrow d^\dagger_\downarrow d_\downarrow
+           + V(d^\dagger_\sigma c_\sigma + h.c.)"""
 
         d_up, d_dw, c_up, c_dw = self.oper
 
-        return - mu*(d_up.T*d_up + d_dw.T*d_dw) + \
-            (e_c - mu)*(c_up.T*c_up + c_dw.T*c_dw) + \
-            u_int*d_up.T*d_up*d_dw.T*d_dw + \
-            hyb*(d_up.T*c_up + d_dw.T*c_dw + c_up.T*d_up + c_dw.T*d_dw)
+        H = {'impurity': d_up.T*d_up + d_dw.T*d_dw,
+             'bath': c_up.T*c_up + c_dw.T*c_dw,
+             'u_int': d_up.T*d_up*d_dw.T*d_dw,
+             'hyb': d_up.T*c_up + d_dw.T*c_dw + c_up.T*d_up + c_dw.T*d_dw}
+
+        return H
 
     def update_H(self, mu, e_c, u_int, hyb):
         """Updates impurity hamiltonian and diagonalizes it"""
-        H = self.hamiltonian(mu, e_c, u_int, hyb)
+        H = - mu*self.H_operators['impurity'] + \
+            (e_c - mu)*self.H_operators['bath'] + \
+            u_int*self.H_operators['u_int'] + \
+            hyb*self.H_operators['hyb']
+
         self.eig_energies, self.eig_states = diagonalize(H.todense())
 
     def expected(self, observable):
@@ -94,7 +106,6 @@ class twosite(object):
                                       d_up_dag, self.beta, self.omega)
         self.GF['Imp G$_0$'] = self.imp_free_gf(mu, e_c, hyb)
         self.GF['$\Sigma$'] = 1/self.GF['Imp G$_0$'] - 1/self.GF['Imp G']
-        self.GF['Lat G'] = self.lattice_gf(mu)
         return np.sqrt(self.imp_z()*self.m2)
 
     def imp_ocupation(self):
@@ -118,28 +129,28 @@ class twosite(object):
         sigma = self.GF['$\Sigma$']
         if self.freq_axis == 'real':
             dw = w[1]-w[0]
-            return 1/(1 - np.mean(np.gradient(sigma.real, dw)[(-dw <= w) * (w <= dw)]))
+            sigma = sigma[(-dw <= w) * (w <= dw)]
+            zet = 1/(1 - (sigma[-1]-sigma[0])/2/dw)
+            return zet
         else:
             dw = 2/self.beta
-            return 1/(1 -np.gradient(sigma.imag[:5], dw)[0])
-
-    def lattice_gf(self, mu):
-        """Compute lattice green function
-
-        .. math:
-            G(\omega) = \int \frac{\rho_0(x) dx}{\omega + i\eta + \mu + x + \Sigma(w)}"""
-        G = []
-        x = np.linspace(-4, 4, 3500)
-        betlat = dos.bethe_lattice(x, self.t)
-        for w, s_w in zip(self.omega, self.GF['$\Sigma$']):
-            integrable = betlat/(w + mu - x - s_w)
-            G.append(simps(integrable, x))
-
-        return np.asarray(G)
+            return 1/(1 - np.gradient(sigma.imag[:5], dw)[0])
 
     def lattice_ocupation(latG, w):
         return -2*simps(latG.imag[w.real <= 0], w.real[w.real <= 0])/np.pi
 
+
+def lattice_gf(sim, mu, wide=5e-3):
+    """Compute lattice green function
+
+    .. math: G(\\omega) = \\int \\frac{\\rho_0(x) dx}{\\omega + i\\eta + \\mu + x + \\Sigma(w)}"""
+    G = []
+    var = sim.omega + mu - sim.GF['$\Sigma$'] + 1j*wide
+    for w in var:
+        integrable = sim.rho_0/(w - sim.x)
+        G.append(simps(integrable, sim.x))
+
+    return np.asarray(G)
 
 def out_plot(sim, spec, label=''):
     w = sim.omega.imag
@@ -161,24 +172,28 @@ def out_plot(sim, spec, label=''):
             plt.plot(w, -1/np.pi*sim.GF['Lat G'].imag, stl, label='A '+label)
             continue
         plt.plot(w, sim.GF[key].real, stl, label='Re {} {}'.format(key, label))
-        plt.plot(w, sim.GF[key].imag, stl, label='Im {} {}'.format(key, label))
+        plt.plot(w, sim.GF[key].imag, stl+'-', label='Im {} {}'.format(key, label))
 
 
 if __name__ == "__main__":
     res = []
-    for U in np.linspace(-4, 4, 3500):
+    for U in [4]:
 
-        sim = twosite(80, 0.5, 'matsubara')
-        hyb = 0.5  # np.sqrt(sim.imp_z()*sim.m2)
+        sim = twosite(50, 0.5, 'real')
+        hyb = 0.32041  # np.sqrt(sim.imp_z()*sim.m2)
         fig = plt.figure()
         for i in range(12):
-            hyb = sim.solve(U/2, U/2, U, hyb)
+            old = hyb
+            hyb = sim.solve(U/2, U/2, U, old)
             out_plot(sim, 'sigma', 'loop {}'.format(i))
+            if np.abs(old - hyb) < 1e-6:
+                break
+
         plt.legend()
         plt.title('U={}, hyb={}'.format(U, hyb))
         plt.ylabel('A($\omega$)')
         plt.xlabel('$\omega$')
-        fig.savefig('Sigma_iw_{:.2f}.png'.format(U), format='png',
-                    transparent=False, bbox_inches='tight', pad_inches=0.05)
-        plt.close(fig)
+#        fig.savefig('Sigma_iw_{:.2f}.png'.format(U), format='png',
+#                    transparent=False, bbox_inches='tight', pad_inches=0.05)
+#        plt.close(fig)
         res.append((U, sim))
