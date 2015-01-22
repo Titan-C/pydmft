2a3481e5cd5c43e397136615ffe3be37748bf7fc
Óscar Nájera <najera.oscar@gmail.com>
2015-01-22 19:43:18 +0100

    Starting documentation

--- qmc-hf/pmhf.py
+++ qmc-hf/pmhf.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 19 15:46:02 2015
+================================
+QMC Hirsch - Fye Impurity solver
+================================
 
-@author: oscar
-
-Translation of QMC Hirsch - Fye
+To treat the Anderson impurity model and solve it using the Hirsch - Fye
+Quantum Monte Carlo algorithm
 """
 import numpy as np
 from scipy.linalg import solve
@@ -13,11 +14,33 @@ Lrang = 2**15
 lfak = 32
 
 
-def matsubara_freq(beta=16., fer=1):
+def matsubara_freq(beta=16., fer=1, Lrang=2**15):
+    """Calculates an array containing the matsubara frequencies under the
+    formula
+
+    .. math:: i\\omega_n = i\\frac{\\pi(2n + f)}{\\beta}
+
+    where :math:`i` is the imaginary unit and :math:`f=1` in the case of
+    fermions, and zero for bosons
+
+    Parameters
+    ----------
+    beta : float
+            Inverse temperature of the system
+    fer : 0 or 1 integer
+            dealing with fermionic particles
+    Lrang : integer
+            size of the array : amount of matsubara frequencies
+
+    Returns
+    -------
+    out : complex ndarray
+
+    """
     return 1j*np.pi*np.arange(-Lrang+fer, Lrang, 2) / beta
 
 
-def greenF(w, sigma=0, mu=0, beta=16., D=1):
+def greenF(w, sigma=0, mu=0, D=1):
     """Calculate green function lattice"""
     Gw = np.zeros(2*Lrang, dtype=np.complex)
     zeta = w - mu - sigma
@@ -48,16 +71,17 @@ def iFFT(gw, beta=16.):
     return gt.real
 
 
-def dyson_sigma(g,g0):
+def dyson_sigma(g, g0, fer=1):
     """Dyson equation for the self energy"""
     sigma = np.zeros(2*Lrang, dtype=np.complex)
-    sigma[1::2] = 1/g0[1::2] - 1/g[1::2]
+    sigma[fer::2] = 1/g0[fer::2] - 1/g[fer::2]
     return sigma
 
-def dyson_g0(g, sigma):
+
+def dyson_g0(g, sigma, fer=1):
     """Dyson equation for the bare Green function"""
     g0 = np.zeros(2*Lrang, dtype=np.complex)
-    g0[1::2] = 1/(1/g[1::2] + sigma[1::2])
+    g0[fer::2] = 1/(1/g[fer::2] + sigma[fer::2])
     return g0
 
 
@@ -70,16 +94,42 @@ def extract_g0t(g0t, lfak=32):
     return np.concatenate((-gt[:-1], gt))
 
 
-def ising_v(lamb, polar=0.5):
-    """initialize the vector v of Ising fields"""
-    vis = np.ones(lfak)
-    rand = np.random.rand(lfak)
+def ising_v(lamb, L=32, polar=0.5):
+    """initialize the vector V of Ising fields
+
+    .. math:: V = \\lambda (\\sigma_1, \\sigma_2, \\cdots, \\sigma_L)
+
+    where the vector entries :math:`\\sigma_n=\\pm 1` are randomized subject
+    to a threshold given by polar
+
+    Parameters
+    ----------
+    lamb : float
+        :math:`\\lambda` factor giving scaling to vector
+    L : integer
+        length of the array
+    polar : float :math:`\\in (0, 1)`
+        polarization threshold, probability of :math:`\\sigma_n=+ 1`
+
+    Returns
+    -------
+    out : single dimension ndarray
+    """
+    vis = np.ones(L)
+    rand = np.random.rand(L)
     vis[rand>polar] = -1
     return vis*lamb
 
 
 
-def impurity(g0):
+def hf_solver(g0, L, sweeps):
+    """Impurity solver call.
+    Takes the propagator :math:`\\mathcal{G}^0(\\tau)` and transforms it
+    into a discretized matrix as
+
+    .. math:: \\mathcal{G}^0_{ij} = \\mathcal{G}^0(i\\Delta\\tau - j\\Delta\\tau)
+
+    """
     g0[0] = -g0[lfak]
 
     gind = lfak + np.arange(lfak).reshape(-1, 1)-np.arange(lfak).reshape(1, -1)
@@ -88,7 +138,7 @@ def impurity(g0):
     gup = gnewclean(gx, 1.)
     gdw = gnewclean(gx, -1.)
 
-    gstup, gstdw = mcs(1000, gup, gdw)
+    gstup, gstdw = mcs(sweeps, gup, gdw)
 
     return wrapup(gstup, gstdw)
 
@@ -140,6 +190,14 @@ def mcs(sweeps, gup, gdw):
 
 
 def gnewclean(gx, sign):
+    """Returns the interacting function
+
+    .. math:: G'_{ij} = B^{-1}_{ij}G_{ij}
+    .. math:: u_j = \\exp(\\sigma v_j) - 1
+    .. math:: B_{ij} = \\delta_{ij} - u_j ( G_{ij} - \\delta_{ij}) \\text{no sumation on } j
+
+
+    """
     ee = np.exp(sign*v) - 1.
     b = np.eye(lfak) - ee * (gx-np.eye(lfak))
 
@@ -166,14 +224,14 @@ dtau, U = 0.5, 2.5
 lamb = np.arccosh(np.exp(dtau*U/2))
 w = matsubara_freq()
 G0w = greenF(w)
-G0t = iFFT(G0w)
-g0t = extract_g0t(G0t)
+
 
 v = ising_v(lamb)
 
 for i in range(4):
-
-    gt = impurity(g0t)
+    G0t = iFFT(G0w)
+    g0t = extract_g0t(G0t)
+    gt = hf_solver(g0t)
     plt.plot(gt, label='it {}'.format(i))
 
     Gt = interpol(gt[lfak:])
@@ -188,7 +246,5 @@ for i in range(4):
     G0w = np.zeros(2*Lrang, dtype=np.complex)
     G0w[1::2] = 1/(w - .25*Gw[1::2])
 
-    G0t = iFFT(G0w)
-    g0t = extract_g0t(G0t)
 
 plt.legend(loc=0)
