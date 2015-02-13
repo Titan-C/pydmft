6ed660f6da74e4af45756fdf4fc6e5953cf17766
Óscar Nájera <najera.oscar@gmail.com>
2015-02-13 01:19:46 +0100

    Test over the fast fourier transforms

---
+++ tests/test_common.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 25 12:44:23 2014
+
+@author: oscar
+"""
+
+from __future__ import division, absolute_import, print_function
+from dmft.common import greenF, gw_invfouriertrans, gt_fouriertrans,\
+ matsubara_freq, fft, ifft
+import numpy as np
+
+def test_fast_fourier_transforms(beta=50.):
+    """Test the fast fourier transforms"""
+    iomega_n = matsubara_freq(beta)
+    gwr = greenF(iomega_n, mu=0.5)
+
+    g_tau = ifft(gwr, beta)
+    g_iomega = fft(g_tau, beta)
+    assert (np.abs(gwr - g_iomega) < 5e-13).all()
+
+
+def test_fourier_trasforms(beta=50., n_tau=1000, n_matsubara=100):
+    """Test the tail improved fourier transforms"""
+    iomega_n = 1j*np.pi*(1+2*np.arange(n_matsubara)) / beta
+    gwr = greenF(iomega_n)[1::2]
+    tau = np.linspace(0, beta, n_tau+1)
+
+    g_tau = gw_invfouriertrans(gwr, tau, iomega_n, beta)
+    g_iomega = gt_fouriertrans(g_tau, tau, iomega_n, beta)
+    assert (np.abs(gwr - g_iomega) < 5e-13).all()
