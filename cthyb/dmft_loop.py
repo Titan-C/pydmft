641908b1a096b6d0a8071e8bf8200e1998aceddf
Oscar Nájera <najera.oscar@gmail.com>
2015-02-23 16:52:25 +0100

    Working in hdf5 to store iterations and hybridization function

--- cthyb/dmft_loop.py
+++ cthyb/dmft_loop.py
@@ -10,11 +10,11 @@ Quantum Monte Carlo algorithm in the hybridization expansion
 import sys
 import numpy as np
 from dmft.common import matsubara_freq, greenF, gw_invfouriertrans
-import shutil
 sys.path.append('/home/oscar/libs/lib')
 
 import pyalps.cthyb as cthyb  # the solver module
 import pyalps.mpi as mpi     # MPI library (required)
+from pyalps.hdf5 import archive
 
 # specify solver parameters
 parms = {
@@ -25,33 +25,48 @@ parms = {
     'N_HISTOGRAM_ORDERS'  : 50,
     'SEED'                : 0,
 
-    'ANTIFERROMAGNET'     : 1,
-    'SYMMETRIZATION'      : 0,
     'N_ORBITALS'          : 2,
-    'DELTA'               : "delta.dat",
+    'DELTA'               : "delta.h5",
+    'DELTA_IN_HDF5'       : 1,
 
-    't'                   : 1,
     'U'                   : 2.0,
     'MU'                  : 1.0,
     'N_TAU'               : 1000,
     'N_MATSUBARA'         : 200,
     'BETA'                : 45,
-    'TEXT_OUTPUT'         : 1,
     'VERBOSE'             : 1,
 }
+
+def save_pm_delta(gtau):
+    save_delta = archive(parms['DELTA'], 'w')
+    gtau = gtau.mean(axis=0)
+    save_delta['/Delta_0'] = gtau
+    save_delta['/Delta_1'] = gtau
+    del save_delta
+
+def recover_g_tau():
+    iteration = archive('results.out.h5', 'r')
+    gtau = []
+    for i in range(2):
+        gtau.append(iteration['G_tau/{}/mean/value'.format(i)])
+    del iteration
+    return np.asarray(gtau)
+
+def save_iter_step(iter_count, g):
+    save = archive('steps.h5', 'w')
+    for i in range(2):
+        save['iter_{}/G_tau/{}/'.format(iter_count, i)] = g[i]
+    del save
 
 
 if mpi.rank == 0:
     iwn = matsubara_freq(parms['BETA'], parms['N_MATSUBARA'])
     tau = np.linspace(0, parms['BETA'], parms['N_TAU']+1)
 
-    giw_u = greenF(iwn, mu=0.5)[1::2]
-    gtau_u = gw_invfouriertrans(giw_u, tau, iwn, parms['BETA'])
+    giw = greenF(iwn, mu=0.)[1::2]
+    gtau = gw_invfouriertrans(giw, tau, iwn, parms['BETA'])
 
-    giw_d = greenF(iwn, mu=-0.5)[1::2]
-    gtau_d = gw_invfouriertrans(giw_d, tau, iwn, parms['BETA'])
-
-    np.savetxt('delta.dat', np.asarray((tau, gtau_u, gtau_d)).T)
+    save_pm_delta(np.asarray((gtau, gtau)))
 
 mpi.world.barrier()
 # solve the impurity model
@@ -60,13 +75,9 @@ mpi.world.barrier()
 for n in range(4):
     cthyb.solve(parms)
     if mpi.rank == 0:
-        shutil.copy('Gt.dat', 'Gt_{}.dat'.format(n))
-        shutil.copy('delta.dat', 'delta_{}.dat'.format(n))
-        shutil.copy('orders.dat', 'orders_{}.dat'.format(n))
-        shutil.copy('simulation.dat', 'simulation_{}.dat'.format(n))
-
-        gtau = np.loadtxt('Gt_{}.dat'.format(n))
+        g_tau = recover_g_tau()
+        save_iter_step(n, g_tau)
         # inverting for AFM self-consistency
-        np.savetxt('delta.dat', np.asarray((tau, gtau[:, 2], gtau[:, 1])).T)
+        save_pm_delta(g_tau)
 
     mpi.world.barrier() # wait until solver input is written
