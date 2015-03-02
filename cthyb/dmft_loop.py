5a3a7b16b6d22c41f057fd9ba9db7424a01c2372
Oscar Nájera <najera.oscar@gmail.com>
2015-03-02 15:46:47 +0100

    IPT solver

--- cthyb/dmft_loop.py
+++ cthyb/dmft_loop.py
@@ -7,6 +7,7 @@ DMFT loop
 To treat the Anderson impurity model and solve it using the continuous time
 Quantum Monte Carlo algorithm in the hybridization expansion
 """
+from __future__ import division, absolute_import, print_function
 import sys
 import numpy as np
 from dmft.common import matsubara_freq, greenF, gw_invfouriertrans
@@ -17,9 +18,9 @@ import pyalps.mpi as mpi     # MPI library (required)
 from pyalps.hdf5 import archive
 
 
-def save_pm_delta(gtau):
+def save_pm_delta(parms, gtau):
     save_delta = archive('delta.h5', 'w')
-    gtau = gtau.mean(axis=0)
+    gtau = parms['t']**2 * gtau.mean(axis=0)
     save_delta['/Delta_0'] = gtau
     save_delta['/Delta_1'] = gtau
     del save_delta
@@ -38,11 +39,11 @@ def save_iter_step(parms, iter_count, g):
         save['iter_{}/G_tau/{}/'.format(iter_count, i)] = g[i]
     del save
 
-def start_delta(beta):
-    iwn = matsubara_freq(beta, 250)
-    tau = np.linspace(0, beta, 1001)
+def start_delta(parms):
+    iwn = matsubara_freq(parms['BETA'], parms['N_MATSUBARA'])
+    tau = np.linspace(0, parms['BETA'], parms['N_TAU']+1)
 
-    giw = greenF(iwn, mu=0.)[1::2]
+    giw = greenF(iwn, mu=0., D=parms['t'])[1::2]
     gtau = gw_invfouriertrans(giw, tau, iwn, beta)
 
     save_pm_delta(np.asarray((gtau, gtau)))
@@ -77,31 +78,32 @@ if __name__ == "__main__":
     BETA = [20.]#[8, 9, 13, 15, 18, 20, 25, 30, 40, 50]
     U =[5.1]# np.arange(1, 7, 0.4)
     for beta in BETA:
-        if mpi.rank == 0:
-            start_delta(beta)
-            print('write delta at beta ', str(beta))
-
         for u_int in U:
             parms = {
-        'SWEEPS'              : 100000000,
-        'THERMALIZATION'      : 1000,
-        'N_MEAS'              : 50,
-        'MAX_TIME'            : 1,
-        'N_HISTOGRAM_ORDERS'  : 50,
-        'SEED'                : 0,
+                'SWEEPS'              : 100000000,
+                'THERMALIZATION'      : 1000,
+                'N_MEAS'              : 50,
+                'MAX_TIME'            : 1,
+                'N_HISTOGRAM_ORDERS'  : 50,
+                'SEED'                : 0,
 
-        'N_ORBITALS'          : 2,
-        'DELTA'               : "delta.h5",
-        'DELTA_IN_HDF5'       : 1,
-        'BASENAME'            : 'PM_b{}_U{}'.format(beta, u_int),
+                'N_ORBITALS'          : 2,
+                'DELTA'               : "delta.h5",
+                'DELTA_IN_HDF5'       : 1,
+                'BASENAME'            : 'PM_b{}_U{}'.format(beta, u_int),
 
-        'U'                   : u_int,
-        'MU'                  : u_int/2.,
-        'N_TAU'               : 1000,
-        'N_MATSUBARA'         : 250,
-        'BETA'                : beta,
-        'VERBOSE'             : 1,
-    }
+                't'                   : 1.
+                'U'                   : u_int,
+                'MU'                  : u_int/2.,
+                'N_TAU'               : 1000,
+                'N_MATSUBARA'         : 250,
+                'BETA'                : beta,
+                'VERBOSE'             : 1,
+            }
+            if mpi.rank == 0 and u_int == U[0]:
+                start_delta(parms)
+                print('write delta at beta ', str(beta))
+
             mpi.world.barrier()
 
             dmft_loop(parms)
---
+++ dmft/ipt_imag.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+r"""
+==========
+IPT Solver
+==========
+
+Within the iterative perturbative theory (IPT) the aim is to express the
+self-energy of the impurity problem as
+
+.. math:: \Sigma(\tau) \approx U^2 \mathcal{G}^0(\tau)^3
+
+the contribution of the Hartree-term is not included here as it is cancelled
+
+"""
+from __future__ import division, absolute_import, print_function
+
+from dmft.common import greenF, gt_fouriertrans, gw_invfouriertrans, \
+    matsubara_freq
+import numpy as np
+import matplotlib.pylab as plt
+
+
+
+def ipt_solver(u_int, g_0_iwn):
+
+    g_0_tau = gw_invfouriertrans(g_0_iwn, tau, iwn, beta)
+    sigma_tau = u_int**2 * g_0_tau**3
+    sigma_iwn = gt_fouriertrans(sigma_tau, tau, iwn, beta)
+    g_iwn = g_0_iwn / (1 - sigma_iwn * g_0_iwn)
+
+    return g_iwn
+
+
+def dmft_loop(loops, u_int, g_iwn):
+    for i in range(loops):
+        g_0_iwn = 1. / (iwn - t**2 * g_iwn)
+        g_iwn = ipt_solver(u_int, g_0_iwn)
+        plt.plot(iwn.imag, g_iwn.imag, '+-', label='it {}'.format(i))
+    return g_iwn
+
+
+beta = 50
+t = 0.5
+tau = np.linspace(0, beta, 1001)
+iwn = matsubara_freq(beta, 400)
+g_iwn = greenF(iwn, D=2*t)[1::2]
+plt.figure()
+g_iwn = dmft_loop(25, 4, g_iwn)
+plt.plot(iwn.imag, (1/iwn).imag)
+plt.ylabel(r'$G(i\omega_n)$')
+plt.xlabel(r'$i\omega_n$')
+plt.title('Matusubara frequency green function')
+plt.ylim([-2, 0])
+plt.legend(loc=0)
