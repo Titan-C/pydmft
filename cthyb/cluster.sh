b8d5da1de4362a5d76754e6ba5b0babb22d49ab1
Oscar Nájera <najera.oscar@gmail.com>
2015-03-02 20:54:26 +0100

    cluster job submission script

---
+++ cthyb/cluster.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -j y
+#$ -S /bin/bash
+
+#$ -q theo-ox.q
+#$ -pe mpi 10
+#$ -N oscardmft
+
+
+MPI_DIR=/opt/openmpi
+
+
+export PATH=~/libs/bin:$PATH
+export LD_LIBRARY_PATH=/home/oscar/libs/lib:$LD_LIBRARY_PATH
+
+export PATH=~/miniconda/bin:$PATH
+
+source activate alps
+$MPI_DIR/bin/mpirun -np $NSLOTS python dmft_loop.py
