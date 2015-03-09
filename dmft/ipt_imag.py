7447cb8af42f795ccda591995cb7a91180ef77de
Oscar Nájera <najera.oscar@gmail.com>
2015-03-09 14:11:01 +0100

    IPT loop independent and own plot example

--- dmft/ipt_imag.py
+++ dmft/ipt_imag.py
@@ -14,41 +14,48 @@ the contribution of the Hartree-term is not included here as it is cancelled
 """
 from __future__ import division, absolute_import, print_function
 
-from dmft.common import greenF, gt_fouriertrans, gw_invfouriertrans, \
-    matsubara_freq
-import numpy as np
-import matplotlib.pylab as plt
+from dmft.common import gt_fouriertrans, gw_invfouriertrans
 
 
 
-def ipt_solver(u_int, g_0_iwn):
+def solver(u_int, g_0_iwn, iwn, tau):
 
-    g_0_tau = gw_invfouriertrans(g_0_iwn, tau, iwn, beta)
+    g_0_tau = gw_invfouriertrans(g_0_iwn, tau, iwn, tau[-1])
     sigma_tau = u_int**2 * g_0_tau**3
-    sigma_iwn = gt_fouriertrans(sigma_tau, tau, iwn, beta)
+    sigma_iwn = gt_fouriertrans(sigma_tau, tau, iwn, tau[-1])
     g_iwn = g_0_iwn / (1 - sigma_iwn * g_0_iwn)
 
     return g_iwn
 
 
-def dmft_loop(loops, u_int, g_iwn):
+def dmft_loop(loops, u_int, t, g_iwn, iwn, tau):
+    """Performs the paramagnetic(spin degenerate) self-consistent loop in a
+    bethe lattice given the input
+
+    Parameters
+    ----------
+    loops : int
+        amount of dmft loops
+    u_int : float
+        Local interation strength
+    t : float
+        Hopping amplitudue between bethe lattice nearest neightbours
+    g_iwn : comples float ndarray
+            Matsubara frequencies starting guess Green function
+    tau : real float ndarray
+            Imaginary time points. Only use the positive range
+    iwn : complex float ndarray
+            fermionic matsubara frequencies. Only use the positive ones
+
+    Returns
+    -------
+    out : complex ndarray
+            Interacting Greens function in matsubara frequencies in every
+            loop step"""
+
+    g_iwn_log = []
     for i in range(loops):
         g_0_iwn = 1. / (iwn - t**2 * g_iwn)
-        g_iwn = ipt_solver(u_int, g_0_iwn)
-        plt.plot(iwn.imag, g_iwn.imag, '+-', label='it {}'.format(i))
-    return g_iwn
-
-
-beta = 50
-t = 0.5
-tau = np.linspace(0, beta, 1001)
-iwn = matsubara_freq(beta, 400)
-g_iwn = greenF(iwn, D=2*t)
-plt.figure()
-g_iwn = dmft_loop(25, 4, g_iwn)
-plt.plot(iwn.imag, (1/iwn).imag)
-plt.ylabel(r'$G(i\omega_n)$')
-plt.xlabel(r'$i\omega_n$')
-plt.title('Matusubara frequency green function')
-plt.ylim([-2, 0])
-plt.legend(loc=0)
+        g_iwn = solver(u_int, g_0_iwn, iwn, tau)
+        g_iwn_log.append(g_iwn)
+    return g_iwn_log
---
+++ examples/plot_ipt.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+r"""
+==========
+IPT Solver
+==========
+
+Within the iterative perturbative theory (IPT) the aim is to express the
+self-energy of the impurity problem as
+
+.. math:: \Sigma(\tau) \approx U^2 \mathcal{G}^0(\tau)^3
+
+the contribution of the Hartree-term is not included here as it is cancelled
+
+"""
+from __future__ import division, absolute_import, print_function
+
+from dmft import ipt_imag
+
+from dmft.common import greenF, matsubara_freq
+import numpy as np
+import matplotlib.pylab as plt
+
+beta = 50
+U = 2
+t = 0.5
+tau = np.linspace(0, beta, 1001)
+iwn = matsubara_freq(beta, 400)
+g_iwn0 = greenF(iwn, D=2*t)
+g_iwn = ipt_imag.dmft_loop(25, U, t, g_iwn0, iwn, tau)[-1]
+
+fig_gw, gw_ax = plt.subplots()
+gw_ax.plot(iwn.imag, g_iwn.real, '+-', label='RE')
+gw_ax.plot(iwn.imag, g_iwn.imag, 's-', label='IM')
+plt.plot(iwn.imag, (1/iwn).imag, label='high w tail ')
+gw_ax.set_xlim([0, 6.5])
+cut = int(6.5*beta/np.pi)
+gw_ax.set_ylim([g_iwn.imag[:cut].min()*1.1, 0])
+plt.legend(loc=0)
+plt.ylabel(r'$G(i\omega_n)$')
+plt.xlabel(r'$i\omega_n$')
+plt.title(r'$G(i\omega_n)$ at $\beta= {}$, $U= {}$'.format(beta, U))
