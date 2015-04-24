f0b483fb679011053c1d00a1a8a6c1306faab756
Óscar Nájera <najera.oscar@gmail.com>
2015-04-24 14:30:14 +0200

    HS impurity solver

--- dmft/hirschfye_HS.py
+++ dmft/hirschfye_HS.py
@@ -9,18 +9,15 @@ Quantum Monte Carlo algorithm
 """
 from __future__ import division, absolute_import, print_function
 import numpy as np
-from scipy.linalg import solve
 from scipy.linalg.blas import dger
-from scipy.interpolate import interp1d
 
-from dmft.common import tau_wn_setup, gw_invfouriertrans, greenF
-import hffast
+import dmft.hirschfye as hf
 
 
 def ising_v(dtau, U, L=32):
     """Initialize the vector of the Hubbard-Stratonovich fields
 
-    .. math:: V = \\Delta \\tau M_l
+    .. math:: V = -\\Delta \\tau M_l
 
     where the vector entries :math:`M_l` are normaly distributed according to
 
@@ -39,4 +36,66 @@ def ising_v(dtau, U, L=32):
     -------
     out : single dimension ndarray
     """
-    return dtau * np.random.normal(0, np.sqrt(U/dtau), L)
+    return -dtau * np.random.normal(0, np.sqrt(U/dtau), L)
+
+
+def imp_solver(g0up, g0dw, v, parms, therm=1000):
+    r"""Impurity solver call. Calcutaltes the interacting Green function
+    as given by the contribution of the auxiliary discretized spin field.
+    """
+
+    gxu = hf.ret_weiss(g0up)
+    gxd = hf.ret_weiss(g0dw)
+    kroneker = np.eye(v.size)
+    gup = hf.gnewclean(gxu, v, 1., kroneker)
+    gdw = hf.gnewclean(gxd, v, -1., kroneker)
+
+    gstup, gstdw = np.zeros_like(gup), np.zeros_like(gdw)
+    for mcs in range(parms['sweeps']+therm):
+        update(gup, gdw, v, parms['U'], parms['dtau_mc'])
+
+        if mcs % therm == 0:
+            gup = hf.gnewclean(gxu, v, 1., kroneker)
+            gdw = hf.gnewclean(gxd, v, -1., kroneker)
+
+        if mcs > therm:
+
+            gstup += gup
+            gstdw += gdw
+
+    gstup = gstup/parms['sweeps']
+    gstdw = gstdw/parms['sweeps']
+
+    return hf.avg_g(gstup), hf.avg_g(gstdw)
+
+
+def update(gup, gdw, v, U, dtau):
+    for j in range(v.size):
+        Vjp = - dtau * np.random.normal(0, np.sqrt(U/dtau), 1)
+        dv = Vjp - v[j]
+        ratup = 1. + (1. - gup[j, j])*(np.exp( dv)-1.)
+        ratdw = 1. + (1. - gdw[j, j])*(np.exp(-dv)-1.)
+        rat = ratup * ratdw
+        gauss_weight = np.exp((Vjp**2-v[j]**2)/(2*U*dtau))
+        rat = rat/(gauss_weight+rat)
+        if rat > np.random.rand():
+            v[j] = Vjp
+            gnew(gup, dv, j)
+            gnew(gdw,-dv, j)
+
+
+def gnew(g, dv, k):
+    """Quick update of the interacting Green function matrix after a single
+    spin flip of the auxiliary field. It calculates
+
+    .. math:: \\alpha = \\frac{\\exp(v'_j - v_j) - 1}
+                        {1 + (1 - G_{jj})(\\exp(v'_j v_j) - 1)}
+    .. math:: G'_{ij} = G_{ij} + \\alpha (G_{ik} - \\delta_{ik})G_{kj}
+
+    no sumation in the indexes"""
+    ee = np.exp(dv)-1.
+    a = ee/(1. + (1.-g[k, k])*ee)
+    x = g[:, k].copy()
+    x[k] -= 1
+    y = g[k, :].copy()
+    g = dger(a, x, y, 1, 1, g, 1, 1, 1)
--- examples/Hirsh-Fye/plot_pm.py
+++ examples/Hirsh-Fye/plot_pm.py
@@ -12,21 +12,23 @@ from __future__ import division, absolute_import, print_function
 
 import matplotlib.pyplot as plt
 import dmft.hirschfye as hf
+import dmft.hirschfye_HS as hfhs
+import numpy as np
 from dmft.common import gt_fouriertrans, gw_invfouriertrans
 
 
 def dmft_loop_pm(gw=None, **kwargs):
     """Implementation of the solver"""
     parameters = {
-                   'n_tau_mc':    40,
+                   'n_tau_mc':    32,
                    'BETA':        16,
                    'N_TAU':    2**11,
                    'N_MATSUBARA': 64,
-                   'U':           2,
+                   'U':           3,
                    't':           0.5,
                    'MU':          0,
-                   'loops':       8,
-                   'sweeps':      20000,
+                   'loops':       1,
+                   'sweeps':      8000,
                   }
 
     tau, w_n, __, Giw, v_aux = hf.setup_PM_sim(parameters)
@@ -37,12 +39,12 @@ def dmft_loop_pm(gw=None, **kwargs):
         Giw = gw
 
     for iter_count in range(parameters['loops']):
-        G0iw = 1/(1j*w_n + parameters['MU'] - parameters['t']**2 * Giw)
+        G0iw = 1/(1j*w_n + parameters['MU'])
         G0t = gw_invfouriertrans(G0iw, tau, w_n)
         g0t = hf.interpol(G0t, parameters['n_tau_mc'])
 
-        gtu, gtd = hf.imp_solver(g0t, g0t, v_aux, parameters['sweeps'])
-        gt = -0.5 * (gtu+gtd)
+        gtu, gtd = hfhs.imp_solver(g0t, g0t, v_aux, parameters)
+        gt = 0.5 * (gtu+gtd)
 
         Gt = hf.interpol(gt, parameters['N_TAU'])
         Giw = gt_fouriertrans(Gt, tau, w_n)
@@ -56,11 +58,13 @@ def dmft_loop_pm(gw=None, **kwargs):
 if __name__ == "__main__":
     sim1 = dmft_loop_pm()
     plt.figure()
+    tau = np.linspace(0, sim1['parameters']['BETA'], sim1['parameters']['n_tau_mc']+1)
     for it in sorted(sim1):
         if 'it' in it:
 #            plt.plot(s['Giw'].real.T, label=it)
-            plt.plot(sim1[it]['gtau'], label=it)
+            plt.semilogy(tau,sim1[it]['gtau'], 'o', label=it)
     plt.legend()
+    print(np.polyfit(tau[:10], np.log(sim1['it00']['gtau'][:10]), 1))
 #    plt.figure()
 #    for it in sorted(sim2):
 #        if 'it' in it:
