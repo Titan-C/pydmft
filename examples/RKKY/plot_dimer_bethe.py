2e786e29a829164d8094677575201e0732692053
Óscar Nájera <najera.oscar@gmail.com>
2015-05-23 22:56:43 +0200

    Imaginary frequencies bethe dimer

--- examples/RKKY/plot_dimer_bethe.py
+++ examples/RKKY/plot_dimer_bethe.py
@@ -38,3 +38,27 @@ for tab in [0, 0.25, 0.5, 0.75, 1.1]:
 plt.legend(loc=0)
 plt.xlabel(r'$\omega$')
 plt.ylabel(r'$A(\omega)$')
+
+
+w_n = gf.matsubara_freq(50., 512)
+iw_n = 1j*w_n
+plt.figure()
+for tab in [0, 0.25, 0.5, 0.75, 1.1]:
+
+    G1 = gf.greenF(w_n, mu=mu-tab, D=2*t)
+    G2 = gf.greenF(w_n, mu=mu+tab, D=2*t)
+
+    Gd = .5*(G1 + G2)
+    Gc = .5*(G1 - G2)
+
+    Dd = iw_n + mu - t2 * Gd
+    Dc = tab + t2 * Gc
+    Gd =  1. / (Dd - Dc**2 / Dd)
+    Gc = -1. / (Dc - Dd**2 / Dc)
+
+    plt.plot(w_n, Gd.imag, 'o-', label=r'$t_c={}$'.format(tab))
+
+plt.legend(loc=0)
+plt.xlim([0, 6.5])
+plt.xlabel(r'$i\omega_n$')
+plt.ylabel(r'$G(i\omega_n)$')
