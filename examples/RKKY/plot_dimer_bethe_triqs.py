a4f4ca0e19203778e18d3d3d3b5554c47354ee46
Óscar Nájera <najera.oscar@gmail.com>
2015-05-24 00:54:49 +0200

    Imaginary frequency dimer free

--- examples/RKKY/plot_dimer_bethe_triqs.py
+++ examples/RKKY/plot_dimer_bethe_triqs.py
@@ -7,9 +7,11 @@ Non interacting dimer of a Bethe lattice
 """
 
 from pytriqs.gf.local import GfImFreq, GfImTime, InverseFourier, \
-Fourier, iOmega_n, inverse, SemiCircular
-#GfReFreq, Omega, Wilson, inverse
-import numpy
+    Fourier, iOmega_n, inverse, SemiCircular
+from pytriqs.gf.local import GfReFreq, Omega
+from pytriqs.plot.mpl_interface import oplot
+import numpy as np
+import matplotlib.pyplot as plt
 
 class IPT_dimer_Solver:
 
@@ -36,25 +38,24 @@ class IPT_dimer_Solver:
         # Dyson equation to get G
         self.g_iw << inverse(inverse(self.g0_iw) - self.sigma_iw)
 
-mu, t = 0.1, 0.5
-t2 = t**2
-tab = 0.1
-beta = 50.
-
-g_iw = GfImFreq(indices=['A', 'B'], beta=beta)
-g_iw['A', 'A'] = iOmega_n + mu
-g_iw['A', 'B'] = -tab
-g_iw['B', 'A'] = -tab
-g_iw['B', 'B'] = iOmega_n + mu
-g_iw << g_iw + t2 * SemiCircular(2*t)
-g_iw.invert()
-
-from pytriqs.gf.local import GfReFreq, Omega, SemiCircular
-from pytriqs.plot.mpl_interface import oplot
 mu, t = 0.0, 0.5
 t2 = t**2
-tab = 0.1
-beta = 50.
+tab = 0.2
+beta = 100.
+
+g_iw = GfImFreq(indices=['A', 'B'], beta=beta)
+gmix = g_iw.copy()
+gmix['A', 'A'] = iOmega_n + mu
+gmix['A', 'B'] = -tab
+gmix['B', 'A'] = -tab
+gmix['B', 'B'] = iOmega_n + mu
+for i in xrange(3):
+
+    g_iw << gmix - t2 * g_iw
+    g_iw.invert()
+    oplot(g_iw['A', 'A'])
+#    oplot(g_iw['B','A'])
+plt.xlim([0, .8])
 
 g_iw = GfReFreq(indices=['A', 'B'], window=(-3,3), n_points = 2**9)
 gmix = g_iw.copy()
@@ -75,7 +76,5 @@ for gam in [0.1, 0.01, 0.001]:
         #
         g_iw.invert()
 
-    oplot(g_iw['A','A'])
-oplot(g_iw['B','B'])
-
-#oplot(g_iw)
+    oplot(g_iw['A', 'A'])
+oplot(g_iw['B', 'B'])
