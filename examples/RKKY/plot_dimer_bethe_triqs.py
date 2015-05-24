42f2d9940d75c7396dbb09c6d26bfd3d13260d36
Óscar Nájera <najera.oscar@gmail.com>
2015-05-24 23:49:26 +0200

    clean triqs bethe dimer

--- examples/RKKY/plot_dimer_bethe_triqs.py
+++ examples/RKKY/plot_dimer_bethe_triqs.py
@@ -66,36 +66,31 @@ t2 = t**2
 tab = 0.3
 beta = 100.
 
-# Matsubara frequency Green's function
-g_iw = GfImFreq(indices=['A', 'B'], beta=beta, n_points=2**9)
-gmix = mix_gf_dimer(g_iw.copy(), iOmega_n, mu, tab)
-
-w_n = gf.matsubara_freq(beta, 512)
-init_gf(g_iw, w_n, mu, tab, t)
-
-for i in xrange(10):
-
-    g_iw << gmix - t2 * g_iw
-    g_iw.invert()
-    oplot(g_iw['A', 'A'], RI='I')
-#    oplot(g_iw['B','A'])
-plt.xlim([0, .8])
 
 # Real frequency spectral function
-g_iw = GfReFreq(indices=['A', 'B'], window=(-3,3), n_points = 2**9)
-gmix = g_iw.copy()
-
-
+plt.figure(2)
 w = 1e-3j+np.linspace(-3, 3, 2**9)
 
-plt.figure()
-for gam in [0.1, 0.01, 0.001]:
-    gmix = mix_gf_dimer(g_iw.copy(), Omega + 1j * gam, mu, tab)
-    init_gf(g_iw, -1j*w, mu, tab, t)
+for tab in [0, 0.25, 0.5, 0.75, 1.1]:
+    g_re = GfReFreq(indices=['A', 'B'], window=(-3, 3), n_points=2**9)
+    gmix_re = mix_gf_dimer(g_re.copy(), Omega + 1e-3j, mu, tab)
 
-    for i in xrange(5):
-        g_iw << gmix - t2 * g_iw
-        g_iw.invert()
+    init_gf(g_re, -1j*w, mu, tab, t)
+    g_re << gmix_re - t2 * g_re
+    g_re.invert()
 
-    oplot(g_iw['A', 'A'], RI='S')
-oplot(g_iw['B', 'B'])
+    oplot(2, g_re['A', 'A'], RI='S', label=r'$t_c={}$'.format(tab))
+
+
+# Matsubara frequency Green's function
+plt.figure(1)
+w_n = gf.matsubara_freq(beta, 512)
+for tab in [0, 0.25, 0.5, 0.75, 1.1]:
+    g_iw = GfImFreq(indices=['A', 'B'], beta=beta, n_points=2**9)
+    gmix = mix_gf_dimer(g_iw.copy(), iOmega_n, mu, tab)
+
+    init_gf(g_iw, w_n, mu, tab, t)
+    g_iw << gmix - t2 * g_iw
+    g_iw.invert()
+    oplot(1, g_iw['A', 'A'], RI='I', label=r'$t_c={}$'.format(tab))
+plt.xlim([0, 6.5])
