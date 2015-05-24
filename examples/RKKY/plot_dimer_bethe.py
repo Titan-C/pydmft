6488bb27d52728e53e3e3dd83f55dfc066c77fb0
Óscar Nájera <najera.oscar@gmail.com>
2015-05-24 22:29:59 +0200

    Modularize non-interacting example bethe dimer

--- examples/RKKY/plot_dimer_bethe.py
+++ examples/RKKY/plot_dimer_bethe.py
@@ -10,25 +10,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dmft.common as gf
 
-w = 1e-3j+np.linspace(-4, 4, 2**10)
 
-mu, t = 0, 0.5
-t2 = t**2
+def non_int_gf(omega_n, mu, tab, t):
+    """Returns the diagonal and out of diagonal Green's function
+    of dimer non interacting bethe lattice"""
 
-plt.figure()
-
-for tab in [0, 0.25, 0.5, 0.75, 1.1]:
-
-    G1 = gf.greenF(-1j*w, mu=mu-tab, D=2*t)
-    G2 = gf.greenF(-1j*w, mu=mu+tab, D=2*t)
+    G1 = gf.greenF(omega_n, mu=mu-tab, D=2*t)
+    G2 = gf.greenF(omega_n, mu=mu+tab, D=2*t)
 
     Gd = .5*(G1 + G2)
     Gc = .5*(G1 - G2)
 
-    Dd = w + mu - t2 * Gd
-    Dc = tab + t2 * Gc
+    return Gd, Gc
+
+
+def invert_gf(Dd, Dc):
+    """Inverts the Greens function in the diagonal and out of diagonal block"""
+
     Gd =  1. / (Dd - Dc**2 / Dd)
     Gc = -1. / (Dc - Dd**2 / Dc)
+
+    return Gd, Gc
+
+
+def self_consistency(omega, Gd, Gc, mu, tab, t2):
+    """Sets the dimer Bethe lattice self consistent condition for the diagonal
+    and out of diagonal block
+    """
+
+    Dd = omega + mu - t2 * Gd
+    Dc = tab + t2 * Gc
+
+    return Dd, Dc
+
+
+# Real frequency spectral function
+w = 1e-3j+np.linspace(-4, 4, 2**10)
+mu, t = 0, 0.5
+t2 = t**2
+
+plt.figure()
+for tab in [0, 0.25, 0.5, 0.75, 1.1]:
+
+    Gd, Gc = non_int_gf(-1j*w, mu, tab, t)
+    Dd, Dc = self_consistency(w, Gd, Gc, mu, tab, t2)
+    Gd, Gc = invert_gf(Dd, Dc)
 
     plt.plot(w.real, -Gd.imag/np.pi, label=r'$t_c={}$'.format(tab))
 #    plt.plot(w.real, Gd.real, label=r'$\Re e Gd$')
@@ -40,21 +66,15 @@ plt.xlabel(r'$\omega$')
 plt.ylabel(r'$A(\omega)$')
 
 
+# Matsubara frequency Green's function
 w_n = gf.matsubara_freq(50., 512)
 iw_n = 1j*w_n
 plt.figure()
 for tab in [0, 0.25, 0.5, 0.75, 1.1]:
 
-    G1 = gf.greenF(w_n, mu=mu-tab, D=2*t)
-    G2 = gf.greenF(w_n, mu=mu+tab, D=2*t)
-
-    Gd = .5*(G1 + G2)
-    Gc = .5*(G1 - G2)
-
-    Dd = iw_n + mu - t2 * Gd
-    Dc = tab + t2 * Gc
-    Gd =  1. / (Dd - Dc**2 / Dd)
-    Gc = -1. / (Dc - Dd**2 / Dc)
+    Gd, Gc = non_int_gf(w_n, mu, tab, t)
+    Dd, Dc = self_consistency(iw_n, Gd, Gc, mu, tab, t2)
+    Gd, Gc = invert_gf(Dd, Dc)
 
     plt.plot(w_n, Gd.imag, 'o-', label=r'$t_c={}$'.format(tab))
 
