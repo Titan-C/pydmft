6e283c92ae6093f805be6b55ec0a32b14e4f8f8f
Óscar Nájera <najera.oscar@gmail.com>
2015-05-26 20:33:29 +0200

    loop on dimer

--- examples/RKKY/plot_dimer_bethe_triqs.py
+++ examples/RKKY/plot_dimer_bethe_triqs.py
@@ -13,6 +13,8 @@ from pytriqs.plot.mpl_interface import oplot
 import dmft.common as gf
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from pytriqs.archive import HDFArchive
 
 
 class IPT_dimer_Solver:
@@ -21,6 +23,7 @@ class IPT_dimer_Solver:
 
         self.U = params['U']
         self.beta = params['beta']
+        self.setup = {}
 
         self.g_iw = GfImFreq(indices=['A', 'B'], beta=self.beta,
                              n_points=params['n_points'])
@@ -66,7 +69,7 @@ def init_gf(g_iw, omega, mu, tab, t):
 mu, t = 0.0, 0.5
 t2 = t**2
 tab = 0.3
-beta = 100.
+beta = 300.
 
 
 # Real frequency spectral function
@@ -98,24 +101,50 @@ plt.ylabel(r'$A(\omega)$')
 plt.title(u'Spectral functions of dimer Bethe lattice at $\\beta/D=100$ and $U/D=0$.\n Analitical continuation Padé approximant')
 
 # Matsubara interacting self-consistency
-w_n = gf.matsubara_freq(beta, 1024)
-for tab in [0, 0.25, 0.5, 0.75, 1.1]:
 
-    S = IPT_dimer_Solver(U=1.5, beta=beta, n_points=len(w_n))
-    gmix = mix_gf_dimer(S.g_iw.copy(), iOmega_n, mu, tab)
+def dimer(tab, t, beta):
+    w_n = gf.matsubara_freq(beta, 1025)
+    S = IPT_dimer_Solver(U=0, beta=beta, n_points=len(w_n))
+    gmix = mix_gf_dimer(S.g_iw.copy(), iOmega_n, 0, tab)
 
-    init_gf(S.g_iw, w_n, mu, tab, t)
+    init_gf(S.g_iw, w_n, 0, tab, t)
 
-    for i in xrange(100):
+    converged = False
+    loops = 0
+    while not converged:
+        oldg = S.g_iw.data.copy()
         # Bethe lattice bath
         S.g0_iw << gmix - t2 * S.g_iw
         S.g0_iw.invert()
-
         S.solve()
-    #    oplot(1, S.g_iw['A', 'A'], RI='I', label=r'$iter {}$'.format(i))
+        converged = np.allclose(S.g_iw.data, oldg, atol=1e-5)
+        loops += 1
 
-    greal = GfReFreq(indices=[1], window=(-4.0, 4.0), n_points=400)
-    greal.set_from_pade(S.g_iw['A', 'A'], 200, 0.0)
-    oplot(greal, RI='S', label=r'$t_c={}$'.format(tab), num=4)
-plt.ylabel(r'$A(\omega)$')
-plt.title(u'Spectral functions of dimer Bethe lattice at $\\beta/D=100$ and $U/D=1.5$.\n Analitical continuation Padé approximant')
+    # Store
+    S.setup.update({'U': U, 't': t, 'tab': tab, 'beta': beta, 'loops': loops})
+
+    groupname = 'U{U}_t{t}_tab{tab}_B{beta}_c{loops}.h5'.format(**S.setup)
+    R = HDFArchive(groupname, 'w')
+    R['g0_tau'] = S.g0_tau
+    R['G_iw'] = S.g_iw
+    R['S_iw'] = S.sigma_iw
+    R['setup'] = S.setup
+    del R
+
+    return True
+
+def dimhelp(args): return dimer(*args)
+
+p=Pool(4)
+conf=[(u,0.5,0.5,150) for u in [0., 0.05, 0.1, 0.15, 0.2, 0.25]]
+
+conf=[(1.5, tab,0.5,150) for tab in [0., 0.25, 0.5, 0.75, 1.]]
+ou = p.map(dimhelp, conf)
+
+
+def plot_re(filen):
+    R = HDFArchive(filen, 'r')
+#
+    greal = GfReFreq(indices=[1], window=(-4.0, 4.0), n_points=256)
+    greal.set_from_pade(R['G_iw']['A', 'A'], 100, 0.0)
+    oplot(-1*greal, RI='I', label=r'$t_c={}$'.format(tab), num=4)
