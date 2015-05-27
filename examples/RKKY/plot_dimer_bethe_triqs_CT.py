dbdfe9281e3c9e2210a02c1ef0d1f7a0aea8f649
Óscar Nájera <najera.oscar@gmail.com>
2015-05-27 18:55:47 +0200

    ct review

--- examples/RKKY/plot_dimer_bethe_triqs_CT.py
+++ examples/RKKY/plot_dimer_bethe_triqs_CT.py
@@ -10,46 +10,22 @@ import sys
 sys.path.append('/home/oscar/libs/lib/python2.7/site-packages/')
 from pytriqs.gf.local import GfImFreq, GfImTime, InverseFourier, \
     Fourier, iOmega_n, inverse
-from pytriqs.gf.local import GfReFreq, Omega
 from pytriqs.operators import n
 
-from pytriqs.plot.mpl_interface import oplot
 import dmft.common as gf
 import numpy as np
-import matplotlib.pyplot as plt
 from pytriqs.archive import *
 import pytriqs.utility.mpi as mpi
+from plot_dimer_bethe_triqs import mix_gf_dimer, init_gf
 
 from pytriqs.applications.impurity_solvers.cthyb import Solver
-
-
-def mix_gf_dimer(gmix, omega, mu, tab):
-    gmix[0, 0] = omega + mu
-    gmix[0, 1] = -tab
-    gmix[1, 0] = -tab
-    gmix[1, 1] = omega + mu
-    return gmix
-
-
-def init_gf(g_iw, omega, mu, tab, t):
-    G1 = gf.greenF(omega, mu=mu-tab, D=2*t)
-    G2 = gf.greenF(omega, mu=mu+tab, D=2*t)
-
-    Gd = .5*(G1 + G2)
-    Gc = .5*(G1 - G2)
-
-    g_iw[0, 0].data[:, 0, 0] = Gd
-    g_iw[0, 1].data[:, 0, 0] = Gc
-    g_iw[1, 0] << g_iw[0, 1]
-    g_iw[1, 1] << g_iw[0, 0]
-
 
 t = 0.5
 D = 2*t
 t2 = t**2
 tab = 0.0
-beta = 25.
-U = 0.
+beta = 75.
+U = 0.5
 mu = U/2.
 
 parms = {'n_cycles': 100000,
@@ -57,33 +33,62 @@ parms = {'n_cycles': 100000,
          'n_warmup_cycles': 1000}
 # Matsubara interacting self-consistency
 
-g_iw = GfImFreq(indices=[0, 1], beta=beta)
+w_n = gf.matsubara_freq(beta, 512)
+g_iw = GfImFreq(indices=['A', 'B'], beta=50, n_points=512)
 gmix = mix_gf_dimer(g_iw.copy(), iOmega_n, mu, tab)
 
 S = Solver(beta=beta, gf_struct={'up': [0, 1], 'down': [0, 1]})
 
 w_n = gf.matsubara_freq(beta, 1025)
-init_gf(S.G_iw['up'], w_n, mu, tab, t)
-S.G_iw['down'] << S.G_iw['up']
+if mpi.is_master_node():
 
-for i in xrange(2):
-    # Bethe lattice bath
-    for na in ['up', 'down']:
-        S.G0_iw[na] << gmix - t2 * S.G_iw[na]
-        S.G0_iw[na].invert()
+    init_gf(S.G_iw['up'], w_n, mu,.0, t)
+    S.G_iw['down'] << S.G_iw['up']
 
-    S.solve(h_loc=U * n('up', 0) * n('down', 0)
-                 +U * n('up', 1) * n('down', 1), **parms)
-
-    oplot(S.G_iw['up']['0','0'], RI='I', label=r'$iter {}$'.format(i))
+converged = False
+loops = 0
 #
-real = GfReFreq(indices=[1], window=(-4.0, 4.0), n_points=400)
-real.set_from_pade(S.G_iw['up']['1','1'],200,0.)
-oplot(real)
-        # Some intermediate saves
-    if mpi.is_master_node():
-      R = HDFArchive("dimer_bethe.h5")
-      R["G_tau-%s"%i] = S.G_tau
-      del R
-plt.ylabel(r'$A(\omega)$')
-plt.title(u'Spectral functions of dimer Bethe lattice at $\\beta/D=100$ and $U/D=1.5$.\n Analitical continuation Padé approximant')
+#if mpi.is_master_node():
+#    newg = 0.5 * (S.G_iw['up'] + S.G_iw['down'])
+#print('uea')
+#while not converged:
+#    # Bethe lattice PM bath
+#    if mpi.is_master_node():
+#        oldg = newg.data.copy()
+#        for na in ['up', 'down']:
+#            S.G0_iw[na] << gmix - t2 * newg
+#        S.G0_iw.invert()
+#    S.G0_iw << mpi.bcast(S.G0_iw)
+##
+##    S.solve(h_loc=U * n('up', 0) * n('down', 0)
+##                 +U * n('up', 1) * n('down', 1), **parms)
+#
+#    mpi.barrier()
+#    if mpi.is_master_node():
+#
+#        newg = 0.5 * (S.G_iw['up'] + S.G_iw['down'])
+#        converged = np.allclose(newg.data, oldg)
+#        loops += 1
+#    converged = mpi.bcast(converged)
+#
+#    mpi.barrier()
+#
+#print(loops)
+#
+#
+##    if mpi.is_master_node():
+##        Results = HDFArchive("dimert0U.5b75.h5")
+##        Results["G_iw%s"%(loops)] = S.G_iw
+##        del Results
+##    oplot(S.G_iw['up']['0','0'], RI='I', label=r'$iter {}$'.format(i))
+###
+##real = GfReFreq(indices=[1], window=(-4.0, 4.0), n_points=400)
+#real.set_from_pade(S.G_iw['up']['1','1'],200,0.)
+#oplot(real)
+#        # Some intermediate saves
+#    if mpi.is_master_node():
+#      R = HDFArchive("dimer_bethe.h5")
+#      R["G_tau-%s"%i] = S.G_tau
+#      del R
+#plt.ylabel(r'$A(\omega)$')
+#plt.title(u'Spectral functions of dimer Bethe lattice at $\\beta/D=100$ and $U/D=1.5$.\n Analitical continuation Padé approximant')
