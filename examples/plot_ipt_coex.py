e6f7166d50c63060495b1c2d9cfc9c8ce8654893
Óscar Nájera <najera.oscar@gmail.com>
2015-05-28 15:27:23 +0200

    python ipt

--- examples/plot_ipt_coex.py
+++ examples/plot_ipt_coex.py
@@ -18,23 +18,23 @@ from dmft.twosite import matsubara_Z
 import numpy as np
 import matplotlib.pylab as plt
 
-U = np.linspace(0, 2.7, 36)
-U = np.concatenate((U, U[-2:11:-1]))
+U = np.linspace(0, 4., 41)
+U = np.concatenate((U, np.linspace(3.8, 2.3, 16)-0.05))
 
-parms = {'MU': 0, 't': 0.5, 'N_TAU': 2**10, 'N_MATSUBARA': 2**9}
+parms = {'MU': 0, 't': 0.5, 'N_TAU': 2**10, 'N_MATSUBARA': 2**10}
 lop_g=[]
-for beta in [16, 25, 50]:
+for beta in [16, 25, 50, 100]:
     u_zet = []
     parms['BETA'] = beta
     tau, w_n = tau_wn_setup(parms)
     g_iwn0 = greenF(w_n, D=2*parms['t'])
     for u_int in U:
-        mix = 0.4 if u_int > 1.5 else 1
+        mix = 0.4 if u_int > 2.5 else 1
         g_iwn, sigma = ipt_imag.dmft_loop(u_int, parms['t'], g_iwn0, w_n, tau, mix)
         lop_g.append(g_iwn)
         u_zet.append(matsubara_Z(sigma.imag, beta))
 
-    plt.plot(U, u_zet, label='$\\beta={}$'.format(beta))
+    plt.plot(U, u_zet, '+-', label='$\\beta={}$'.format(beta))
 plt.title('Hysteresis loop of the quasiparticle weigth')
 plt.legend()
 plt.ylabel('Z')
