b3a2c7da7d0ba40cea66f58e052e7efc0a4d42f5
Óscar Nájera <najera.oscar@gmail.com>
2015-06-01 19:36:55 +0200

    organize dimer calcs

--- examples/RKKY/dmft_RKKY_dimer_IPT.py
+++ examples/RKKY/dmft_RKKY_dimer_IPT.py
@@ -5,43 +5,43 @@ Created on Fri May 29 13:51:22 2015
 @author: oscar
 """
 
-import sys
-sys.path.append('/home/oscar/libs/lib/python2.7/site-packages')
-import dmft.common as gf
-from plot_dimer_bethe_triqs import mix_gf_dimer, init_gf_met, init_gf_ins
-from RKKY_dimer_IPT import Dimer_Solver, dimer
-
 from pytriqs.gf.local import iOmega_n
-
+import dmft.common as gf
 import numpy as np
+from multiprocessing import Pool
+from dmft.RKKY_dimer_IPT import mix_gf_dimer, init_gf_met, init_gf_ins, \
+    Dimer_Solver, dimer
+
 
 # Matsubara interacting self-consistency
-def loop_u(urange, tab, t, beta, imet):
-    w_n = gf.matsubara_freq(beta, 2025)
+def loop_u(urange, tab, t, beta, file_str):
+    w_n = gf.matsubara_freq(beta, 1025)
     S = Dimer_Solver(U=0, beta=beta, n_points=len(w_n))
     gmix = mix_gf_dimer(S.g_iw.copy(), iOmega_n, 0, tab)
-
     S.setup.update({'t': t, 'tab': tab, 'beta': beta})
-    file_label = '_fuloop_t{t}_tab{tab}_B{beta}.h5'.format(**S.setup)
-    filename = None
-    if imet == 'metal':
+
+    if file_str.startswith('met'):
         init_gf_met(S.g_iw, w_n, 0, tab, t)
-        filename = 'met'+file_label
     else:
         init_gf_ins(S.g_iw, w_n, 0, tab, urange[0])
-        filename = 'ins'+file_label
 
     for u_int in urange:
         S.U = u_int
-
-        step = '/U{}/'.format(S.U)
-        dimer(S, gmix, filename, step)
+        dimer(S, gmix, file_str, '/U{U}/')
 
     return True
 
 #
-tabra = np.arange(0, 1.3, 0.05)
 
-met = [loop_u(np.arange(0, 6, 0.01), tab, 0.5, 20., 'metal') for tab in tabra]
+#met = [loop_u(np.arange(0, 6, 0.1), tab, 0.5, 64., 'metal') for tab in tabra]
 
 #ins = [loop_u(np.arange(6, 0, -0.01), tab, 0.5, 20., 'ins') for tab in tabra]
+def dimhelp(tab):
+    return loop_u(np.arange(0, 4.5, 0.05), tab, 0.5, 100., 'met_fuloop_t{t}_tab{tab}_B{beta}.h5')
+
+
+p = Pool(6)
+tabra = np.arange(0, 1.3, 0.05)
+
+
+ou = p.map(dimhelp, tabra.tolist())
