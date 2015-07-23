e5c9fe3671b7913fd04c0bf79bd4deee952a04b8
Óscar Nájera <najera.oscar@gmail.com>
2015-07-23 14:00:33 +0200

    separate script for hf dimer local run/dev with cluster jobs

--- examples/Hirsh-Fye/plot_dimer_pm.py
+++ examples/Hirsh-Fye/plot_dimer_pm.py
@@ -10,7 +10,7 @@ Quantum Monte Carlo algorithm for a paramagnetic impurity
 
 from __future__ import division, absolute_import, print_function
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import dmft.hirschfye as hf
 import numpy as np
 import dmft.common as gf
@@ -18,8 +18,7 @@ from pytriqs.gf.local import GfImFreq, GfImTime, InverseFourier, \
     Fourier, inverse, TailGf, iOmega_n, inverse
 import dmft.RKKY_dimer_IPT as rt
 from pytriqs.plot.mpl_interface import oplot
-import argparse
-from joblib import Parallel, delayed
+
 
 
 def dmft_loop_pm(urange, tab, t, tn, beta, file_str, **params):
@@ -38,24 +37,24 @@ def dmft_loop_pm(urange, tab, t, tn, beta, file_str, **params):
                'SITES': 2,
                'loops':       0,  # starting loop count
                'max_loops':   20,
-               'sweeps':      int(2e6),
-               'therm':       int(1e5),
+               'sweeps':      int(2e4),
+               'therm':       int(5e3),
                'N_meas':      3,
                'save_logs':   False,
                'updater':     'discrete',
                'convegence_tol': 4e-3,
               }
+    setup.update(params)
 
     try:  # try reloading data from disk
         last_run = rt.HDFArchive(file_str.format(**setup), 'r')
-        lastU = last_run.keys()[-1]
+        lastU = 'U'+str(urange[0])
         lastit = last_run[lastU].keys()[-1]
         setup = last_run[lastU][lastit]['setup']
         setup.update(params)
         S = rt.Dimer_Solver_hf(**setup)
         rt.load_gf(S.g_iw, last_run[lastU][lastit]['G_iwd'],
                    last_run[lastU][lastit]['G_iwo'])
-        urange = [u for u in urange if u >= float(lastU[1:])]
         del last_run
     except Exception:  # if no data clean start
         S = rt.Dimer_Solver_hf(**setup)
@@ -69,6 +68,7 @@ def dmft_loop_pm(urange, tab, t, tn, beta, file_str, **params):
 
     for u_int in urange:
         S.U = u_int
+        S.setup['max_loops'] = setup['loops'] + setup['max_loops']
         S.V_field = hf.ising_v(S.setup['dtau_mc'], S.U,
                                L=S.setup['SITES']*S.setup['n_tau_mc'])
         dimer_loop(S, gmix, tau, file_str, '/U{U}/')
@@ -100,6 +100,7 @@ def dimer_loop(S, gmix, tau, filename, step):
                                 atol=S.setup['convegence_tol'])
         loop_count = S.setup['loops'] + 1
         S.setup.update({'U': S.U, 'loops': loop_count})
+        print(S.setup, loop_count)
         rt.store_sim(S, filename, step+'it{:02}/'.format(loop_count))
 
         max_dist = np.max(abs(S.g_iw.data - oldg))
@@ -114,22 +115,38 @@ def dimer_loop(S, gmix, tau, filename, step):
             converged = True
 
 
+def plot_gf_loops(tab, beta, filestr, nf):
+    R = rt.HDFArchive(filestr.format(tab, beta), 'r')
+    w_n = gf.matsubara_freq(beta, 2*nf)
+    for ru in R.keys():
+        diag_f = []
+        offdiag_f = []
+        f, ax = plt.subplots(1, 2, figsize=(18, 8), sharex=True)
+        f.subplots_adjust(hspace=0.2)
+        gfin = f.add_axes([0.16, 0.17, 0.20, 0.25])
+        for u_iter in R[ru].keys():
+            if 'it' in u_iter:
+                diag_f.append(R[ru][u_iter]['G_iwd'].data[:nf, 0, 0].imag)
+                offdiag_f.append(R[ru][u_iter]['G_iwo'].data[:nf, 0, 0].real)
+                gfin.plot(w_n, R[ru][u_iter]['G_iwd'].data[:2*nf, 0, 0].imag, 'bs:')
+                gfin.plot(w_n, R[ru][u_iter]['G_iwo'].data[:2*nf, 0, 0].real, 'gs:')
+
+        diag_f = np.asarray(diag_f).T
+        offdiag_f = np.asarray(offdiag_f).T
+        gfin.set_xticks(gfin.get_xlim())
+        gfin.set_yticks(gfin.get_ylim())
+        plt.axhline()
+        for freq, (hd, ho) in enumerate(zip(diag_f, offdiag_f)):
+            ax[0].plot(hd,'o-.', label='n='+str(freq+1))
+            ax[1].plot(ho,'o-.', label='n='+str(freq+1))
+        ax[1].legend(loc=3,prop={'size':18})
+        plt.suptitle('First frequencies of the Matsubara GF, at iteration @ U/D={} $t_{{ab}}/D={}$ $\\beta D={}$'.format(ru, tab, beta))
+        #show()
+        #close()
+    del R
 if __name__ == "__main__":
-#    dmft_loop_pm([2.], 0.2, 0.5, 0., 36., 'metf_HF_Ul_dt0.3_t{t}_tp{tp}_B{BETA}.h5',
-#                 dtau_mc=1., sweeps=5000, max_loops=2)
-    parser = argparse.ArgumentParser(description='DMFT loop for a dimer bethe lattice solved by IPT')
-    parser.add_argument('beta', metavar='B', type=float,
-                        default=16., help='The inverse temperature')
-    parser.add_argument('-r','--restart', action='store_true',
-                        help='Restart the job using data from disk')
+    dmft_loop_pm([2.7], 0.2, 0.5, 0., 36., '1metf_HF_Ul_dt0.3_t{t}_tp{tp}_B{BETA}.h5',
+                 dtau_mc=.4, sweeps=10000, max_loops=10)
 
-
-    tabra = [.18, 0.22, 0.25, 0.27]
-    args = parser.parse_args()
-    BETA = args.beta
-
-    ur = np.arange(2, 3, 0.1)
-    Parallel(n_jobs=4, verbose=5)(delayed(dmft_loop_pm)(ur,
-         tab, 0.5, 0., BETA, 'disk/metf_HF_Ul_t{t}_tp{tp}_B{BETA}.h5')
-         for tab in tabra)
+    plot_gf_loops(0.2, 36., '1metf_HF_Ul_dt0.3_t0.5_tp{}_B{}.h5', 5 )
 
