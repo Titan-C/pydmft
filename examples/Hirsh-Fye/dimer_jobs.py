6f14afc2c760cace8b9e31ad142903894280ca44
Oscar Nájera <najera.oscar@gmail.com>
2015-07-23 18:03:03 +0200

    cluter jobs and correct import

--- examples/Hirsh-Fye/dimer_jobs.py
+++ examples/Hirsh-Fye/dimer_jobs.py
@@ -7,7 +7,7 @@ Created on Thu Jul 23 13:03:46 2015
 import argparse
 import numpy as np
 from joblib import Parallel, delayed
-from plot_dimer import dmft_loop_pm
+from plot_dimer_pm import dmft_loop_pm
 
 parser = argparse.ArgumentParser(description='DMFT loop for a dimer bethe\
                                               lattice solved by IPT')
@@ -15,7 +15,7 @@ parser.add_argument('beta', metavar='B', type=float,
                     default=16., help='The inverse temperature')
 
 PARAMS = {'N_TAU':    2**13,
-          'dtau_mc': 0.5,
+          'dtau_mc': 1.,
           'U':           0.,
           'MU':          0.,
           'BANDS': 1,
@@ -35,7 +35,7 @@ args = parser.parse_args()
 BETA = args.beta
 
 ur = np.arange(2, 3, 0.1)
-Parallel(n_jobs=4, verbose=5)(delayed(dmft_loop_pm)(ur,
+Parallel(n_jobs=6, verbose=5)(delayed(dmft_loop_pm)(ur,
                              tab, 0.5, 0., BETA,
                              'disk/metf_HF_Ul_tp{tp}_B{BETA}.h5')
                              for tab in TABRA)
