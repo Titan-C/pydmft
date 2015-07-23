d868348baa899192d091ef1ad356b3cb4f1165c5
Óscar Nájera <najera.oscar@gmail.com>
2015-07-23 11:05:55 +0200

    restart jobs

--- examples/Hirsh-Fye/plot_dimer_pm.py
+++ examples/Hirsh-Fye/plot_dimer_pm.py
@@ -58,6 +58,27 @@ def dmft_loop_pm(urange, tab, t, tn, beta, file_str):
         S.V_field = hf.ising_v(S.setup['dtau_mc'], S.U, L=S.setup['SITES']*S.setup['n_tau_mc'])
         dimer_loop(S, gmix, tau, file_str, '/U{U}/')
 
+def restartt_dmft(file_str, urange, **params):
+    last_run = rt.HDFArchive(file_str, 'r')
+    lastU = last_run.keys()[-1]
+    lastit = last_run[lastU].keys()[-1]
+    setup = last_run[lastU][lastit]['setup']
+    setup.update(params)
+
+    S = rt.Dimer_Solver_hf(**setup)
+    gmix = rt.mix_gf_dimer(S.g_iw.copy(), iOmega_n, setup['MU'], setup['tp'])
+    rt.load_gf(S.g_iw, last_run[lastU][lastit]['G_iwd'],
+               last_run[lastU][lastit]['G_iwo'])
+    del last_run
+    nurange = [u for u in urange if u>=float(lastU[1:])]
+    for u_int in nurange:
+        S.U = u_int
+#        S.setup['dtau_mc'] = min(0.5, 0.3/S.U)
+        tau = np.arange(0, S.setup['BETA'], S.setup['dtau_mc'])
+        S.setup['n_tau_mc'] = len(tau)
+        S.V_field = hf.ising_v(S.setup['dtau_mc'], S.U, L=S.setup['SITES']*S.setup['n_tau_mc'])
+        dimer_loop(S, gmix, tau, file_str, '/U{U}/', setup['loops'])
+
 def get_selfE(G_iwd, G_iwo):
     nf = len(G_iwd.mesh)
     beta = G_iwd.beta
@@ -68,9 +89,8 @@ def get_selfE(G_iwd, G_iwo):
     sigma << gmix  - 0.25*g_iw - inverse(g_iw)
     return sigma
 
-def dimer_loop(S, gmix, tau, filename, step):
+def dimer_loop(S, gmix, tau, filename, step, loop_count=0):
     converged = False
-    loops = 0
     while not converged:
 #    for i in range(12):
         # Enforce DMFT Paramagnetic
@@ -83,32 +103,31 @@ def dimer_loop(S, gmix, tau, filename, step):
         S.solve(tau)
 
         converged = np.allclose(S.g_iw.data, oldg, atol=4e-3)
-        loops += 1
-        S.setup.update({'U': S.U, 'loops': loops})
-        rt.store_sim(S, filename, step+'it{:02}/'.format(loops))
+        loop_count += 1
+        S.setup.update({'U': S.U, 'loops': loop_count})
+        rt.store_sim(S, filename, step+'it{:02}/'.format(loop_count))
 
         max_dist = np.max(abs(S.g_iw.data - oldg))
-        print('B', S.beta, 'tp', S.setup['tp'], 'U:', S.U, 'l:', loops,
+        print('B', S.beta, 'tp', S.setup['tp'], 'U:', S.U, 'l:', loop_count,
               converged, max_dist)
 
 #        ct = '-' if loops<8 else '+--'
 #        oplot(S.g_iw['A', 'A'], ct, RI='I', label='d'+str(loops), num=6)
 #        oplot(S.g_iw['A', 'B'], ct, RI='R', label='o'+str(loops), num=7)
 
-        if loops > 20:
+        if loop_count > 20:
             converged = True
 
     rt.store_sim(S, filename, step)
 
 if __name__ == "__main__":
-#    sim = dmft_loop_pm([1.], 0.2, 0.5, 0., 16., 'B16t.4.h5')
-#    plt.figure()
-#    for it in sorted(sim):
-#        if 'it' in it:
-#            oplot(sim[it]['Giw']['B','B'], lw=2)
+#    restartt_dmft('metf_HF_Ul_dt0.3_t0.5_tp0.2_B36.0.h5', [2.], sweeps=5000, dtau_mc=1. )
+
     parser = argparse.ArgumentParser(description='DMFT loop for a dimer bethe lattice solved by IPT')
     parser.add_argument('beta', metavar='B', type=float,
                         default=16., help='The inverse temperature')
+    parser.add_argument('-r','--restart', action='store_true',
+                        help='Restart the job using data from disk')
 #
 #
     tabra = [.18, 0.22, 0.25, 0.27]
