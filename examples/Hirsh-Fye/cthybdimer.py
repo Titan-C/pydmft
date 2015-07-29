8c33c141927e6c7b260feef5e255147461a3f45d
Óscar Nájera <najera.oscar@gmail.com>
2015-07-29 15:10:59 +0200

    cthyb dimer as a function

--- examples/Hirsh-Fye/cthybdimer.py
+++ examples/Hirsh-Fye/cthybdimer.py
@@ -19,7 +19,7 @@ params = {
     'n_warmup_cycles' : int(5e4),
     'move_double': True,
     'measure_g_l': True,
-    'measure_pert_order': True,
+#    'measure_pert_order': True,
     'random_seed': int(time()/2**14+time()/2**16*mpi.rank)
 }
 
@@ -46,33 +46,38 @@ def mix_gf_dimer(gmix, omega, mu, tab):
     return gmix
 
 
-u = 'U2.6'
-file_str = 'disk/metf_HF_Ul_tp0.21_B10.0.h5'
+def cthyb_last_run(u_int, tp, BETA, file_str):
+    u = 'U'+str(u_int)
+    with rt.HDFArchive(file_str.format(tp, BETA)) as last_run:
+        lastit = mpi.bcast(last_run[u].keys()[-1])
+        setup = mpi.bcast(last_run[u][lastit]['setup'])
+        gd = mpi.bcast(last_run[u][lastit]['G_iwd'])
+        go = mpi.bcast(last_run[u][lastit]['G_iwo'])
 
-with rt.HDFArchive(file_str) as last_run:
-    lastit = mpi.bcast(last_run[u].keys()[-1])
-    setup = mpi.bcast(last_run[u][lastit]['setup'])
-    gd = mpi.bcast(last_run[u][lastit]['G_iwd'])
-    go = mpi.bcast(last_run[u][lastit]['G_iwo'])
+    S = Solver(beta=setup['BETA'], gf_struct={'up': [0, 1], 'down': [0, 1]},
+               n_iw=setup['n_points'], n_tau=1025, n_l=15)
 
-S = Solver(beta=setup['BETA'], gf_struct={'up': [0, 1], 'down': [0, 1]},
-           n_iw=setup['n_points'], n_tau=1025, n_l=15)
+    for name, gblock in S.G_iw:
+        load_gf(gblock, gd, go)
 
-for name, gblock in S.G_iw:
-    load_gf(gblock, gd, go)
+    U = setup['U']
+    gmix = mix_gf_dimer(S.G_iw['up'].copy(), iOmega_n, U/2., setup['tp'])
+    for name, g0block in S.G0_iw:
+        g0block << inverse(gmix - 0.25*S.G_iw['up'])
 
-U = setup['U']
-gmix = mix_gf_dimer(S.G_iw['up'].copy(), iOmega_n, U/2., setup['tp'])
-for name, g0block in S.G0_iw:
-    g0block << inverse(gmix - 0.25*S.G_iw['up'])
+    S.solve(h_int=U * n('up',0) * n('down',0) + U * n('up',1) * n('down',1), **params)
 
-S.solve(h_int=U * n('up',0) * n('down',0) + U * n('up',1) * n('down',1), **params)
-
-if mpi.is_master_node():
-    with rt.HDFArchive(file_str.format(**setup), 'r') as last_run:
-        last_run[u]['cthyb/G_iw'] = S.G_iw
-        last_run[u]['cthyb/G_tau'] = S.G_tau
-        last_run[u]['cthyb/G_l'] = S.G_l
+    if mpi.is_master_node():
+        with rt.HDFArchive(file_str.format(**setup), 'r') as last_run:
+            last_run[u]['cthyb/G_iw'] = S.G_iw
+            last_run[u]['cthyb/G_tau'] = S.G_tau
+            last_run[u]['cthyb/G_l'] = S.G_l
 
 
+TABRA = np.arange(0.18, 0.3, 0.01)
+ur = np.arange(2, 3, 0.1)
+
+for tab in TABRA:
+    for u_int in ur:
+        cthyb_last_run(u_int, tab, 10., 'disk/metf_HF_Ul_tp{}_B{}.h5')
 
