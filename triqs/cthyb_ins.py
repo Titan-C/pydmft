c253505870a1a33fd05d34cce64d9e9eabf249e8
Oscar Nájera <najera.oscar@gmail.com>
2015-08-17 18:48:51 +0200

    tail fits to higher momentum order in single band DMFT

--- triqs/cthyb_ins.py
+++ triqs/cthyb_ins.py
@@ -15,7 +15,7 @@ U = 3.2
 half_bandwidth = 1.0
 chemical_potential = U/2.0
 beta = 100.
-n_loops = 1
+n_loops = 5
 
 # Construct the CTQMC solver
 from pytriqs.applications.impurity_solvers.cthyb import Solver
@@ -35,9 +35,10 @@ params = {'n_cycles': int(3e7),
 g_iw = GfImFreq(indices = [0], beta = beta, n_points=1025)
 #g_iw << SemiCircular(half_bandwidth)
 g_iw.data[:,0,0] = np.load('Giw_out.npy')[-1] #np.load('fgiws500.npy')
-fixed=TailGf(1,1,3,-1)
+fixed=TailGf(1,1,5,-1)
 fixed[1]=np.array([[1]])
-g_iw.fit_tail(fixed,4, 300, 1025)
+fixed[3]=np.array([[3.2**2/4]])
+g_iw.fit_tail(fixed,5, 98, 1025)
 
 #R = HDFArchive("compareHF.h5")
 for name, g0block in S.G_tau:
@@ -64,22 +65,22 @@ for it in range(n_loops):
     # Compute S.G0_iw with the self-consistency condition while imposing paramagnetism
 #    g_iw.set_from_legendre( 0.5 * ( S.G_l['up'] + S.G_l['down'] ))
     g_iw << Fourier(0.5*(S.G_tau['up']+S.G_tau['down']))
-    g_iw.fit_tail(fixed,3,300,1025)
-    g_iw.data[:,0,0]=1j*g_iw.data[:,0,0].imag
+    g_iw.fit_tail(fixed, 5, 98, 1025)
+    g_iw.data[:]=1j*g_iw.data.imag
 
-    g=np.squeeze(g_iw.data[:,0,0])
+    g=np.squeeze(g_iw.data)
     den=np.abs(1j*wn +1.6-.25*g)**2
     g0iw=(1.6-.25*g.real)/den-1j*(wn-.25*g.imag)/den
     for name, g0 in S.G0_iw:
 #        g0 << inverse( iOmega_n + chemical_potential - (half_bandwidth/2.0)**2  * g_iw )
         g0.data[:,0,0] = g0iw
-        g0.fit_tail(fixedg0, 6, 300, 1025)
+        g0.fit_tail(fixedg0, 6, 100, 1025)
     # Run the solver
     S.solve(h_int=U * n('up',0) * n('down',0), **params)
 
     # Some intermediate saves
     if mpi.is_master_node():
-        with HDFArchive("deep_in_ins2.h5") as R:
+        with HDFArchive("deep_in_ins3.h5") as R:
           R["G_tau-%s"%it] = S.G_tau
           R["G_iw-%s"%it] = S.G_iw
           R["G0_iw-%s"%it] = S.G0_iw
