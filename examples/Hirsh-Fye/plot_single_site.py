2cb857af01760b4e4a2885ac546a98f8fe1733eb
Óscar Nájera <najera.oscar@gmail.com>
2015-09-07 11:46:38 +0200

    Plotter of DOS estimation

--- examples/Hirsh-Fye/plot_single_site.py
+++ examples/Hirsh-Fye/plot_single_site.py
@@ -13,6 +13,7 @@ from pytriqs.archive import HDFArchive
 import dmft.common as gf
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 plt.matplotlib.rcParams.update({'figure.figsize': (8, 8), 'axes.labelsize': 22,
                                 'axes.titlesize': 22})
 
@@ -42,6 +43,68 @@ def show_conv(beta, U, filestr='SB_PM_B{}.h5', nf=5, xlim=2):
     ax[1].set_title('Evolution of the first frequencies')
     ax[1].set_ylabel(graf+'$(l)$')
     ax[1].set_xlabel('iterations')
+
+    plt.show()
+    plt.close()
+
+
+def _averager(it_output, last_iterations):
+    """Averages over the files terminating with the numbers given in vector"""
+    sgiw = 0
+    for it in last_iterations:
+        giw = it_output[it]['giw']
+        sgiw += giw
+
+    sgiw /= len(last_iterations)
+    sgiw.real = 0.
+    return sgiw
+
+
+def fit_dos(beta, avg, filestr='SB_PM_B{}.h5'):
+    """Fits for all Green's functions at a given beta their
+    density of states at the Fermi energy
+
+    Parameters
+    ----------
+    beta: float inverse temperature
+    avg: int number of last iterations to average over
+
+    Return
+    ------
+    arrays of Interaction, Fitted and source GF imaginary part
+    """
+
+    U = []
+    figiw = []
+    lgiw = []
+    w_n = gf.matsubara_freq(beta, 3)
+
+    with HDFArchive(filestr.format(beta), 'r') as output_files:
+        for u_str in sorted(output_files.keys()):
+            U.append(float(re.findall("U([\d\.]+)", u_str)[0]))
+            last_iterations = sorted(output_files[u_str].keys())[-avg:]
+            giw = _averager(output_files[u_str], last_iterations)
+
+            gfit = gf.fit_gf(w_n, giw.imag)
+            figiw.append(gfit)
+            lgiw.append(giw)
+
+    return np.asarray(U), figiw, lgiw
+
+
+def plot_fit_dos(beta, avg, filestr='B{}_U{}/Gf.out.*', xlim=2):
+    """Plot the evolution of the Green's function in DMFT iterations"""
+    f, ax = plt.subplots(1, 2, figsize=(13, 8))
+
+    U, fit_gf, lgiw = fit_dos(beta, avg)
+    wn = gf.matsubara_freq(beta)
+    wr = np.arange(0, wn[2], 0.05)
+    for u, gfit, igiw in zip(U, fit_gf, lgiw):
+        ax[0].plot(wn, igiw, 'o:', label='U='+str(u))
+        ax[0].plot(wr, gfit(wr), 'k:')
+
+    ax[0].set_xlim([0, xlim])
+    ax[1].plot(U, [dos(0) for dos in fit_gf], 'o-')
 
     plt.show()
     plt.close()
