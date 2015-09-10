8fff4e4cb2da21c602d5f8db487dcabdaf9c4c07
Óscar Nájera <najera.oscar@gmail.com>
2015-09-10 13:55:47 +0200

    File moving and renaming

--- examples/plot_bethe.py
+++ examples/plot_bethe.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Bethe lattice semi-eliptical DOS
+Bethe lattice semi-elliptical DOS
 ================================
 
-The Bethe lattice has a simpl form of the self-consistency. This plots how
+The Bethe lattice has a simple form of the self-consistency. This plots hoomega
 this equation includes iteratively the poles is the Green function so one
 recovers such density of states.
 """
@@ -11,16 +11,16 @@ recovers such density of states.
 import numpy as np
 import matplotlib.pyplot as plt
 
-w = 1e-3j+np.linspace(-4, 4, 800)
-gr = np.zeros_like(w)
+omega = 1e-3j+np.linspace(-4, 4, 800)
+gf_w = np.zeros_like(omega)
 t = 0.5
 
 for i in xrange(1, int(1e4+1)):
-    gr = 1/(w - t**2 * gr)
+    gf_w = 1/(omega - t**2 * gf_w)
     if i in [1, 4, 10000]:
-        plt.plot(w.real, gr.real, label='iteration '+str(i))
+        plt.plot(omega.real, gf_w.real, label='iteration '+str(i))
 
-plt.plot(w.real, -gr.imag/np.pi, label=r'$A(\omega)$')
+plt.plot(omega.real, -gf_w.imag/np.pi, label=r'$A(\omega)$')
 plt.legend(loc=0)
 plt.ylim([-3, 3])
 plt.xlabel(r'$\omega$')
