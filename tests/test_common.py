fdefd56326b97a5eea50bc00ce2006230dd2708b
Óscar Nájera <najera.oscar@gmail.com>
2015-09-10 23:22:26 +0200

    Test on fitting green function for extrapolation

--- tests/test_common.py
+++ tests/test_common.py
@@ -7,7 +7,7 @@ Created on Tue Nov 25 12:44:23 2014
 
 from __future__ import division, absolute_import, print_function
 from dmft.common import greenF, gw_invfouriertrans, gt_fouriertrans,\
- tau_wn_setup
+ tau_wn_setup, matsubara_freq, fit_gf
 import numpy as np
 import pytest
 
@@ -17,9 +17,17 @@ def test_fourier_trasforms(chempot, beta=50., n_tau=2**11, n_matsubara=115):
     """Test the tail improved fourier transforms"""
     parms = {'BETA': beta, 'N_TAU': n_tau, 'N_MATSUBARA': n_matsubara}
     tau, w_n = tau_wn_setup(parms)
-    gw = greenF(w_n, mu=chempot)
+    giw = greenF(w_n, mu=chempot)
 
-    for gwr in [gw, np.array([gw, gw])]:
+    for gwr in [giw, np.array([giw, giw])]:
         g_tau = gw_invfouriertrans(gwr, tau, w_n)
         g_iomega = gt_fouriertrans(g_tau, tau, w_n)
         assert np.allclose(gwr, g_iomega)
+
+
+def test_fit_gf():
+    """Test the interpolation of Green function in Bethe Lattice"""
+    w_n = matsubara_freq(100, 3)
+    giw = greenF(w_n).imag
+    cont_g = fit_gf(w_n, giw)
+    assert abs(cont_g(0.) + 2.) < 1e-4
