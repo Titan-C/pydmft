45ddfddd5a3b452dfc7323e2b58985ad1c28484e
Óscar Nájera <najera.oscar@gmail.com>
2015-09-15 13:09:30 +0200

    load matplotlib early

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 """testing the examples before gallery"""
 from __future__ import division, print_function, absolute_import
-import dmft.plot.hf_single_site as pss
-import matplotlib
 import subprocess
+# matplotlib back end has to be called before it gets loaded elsewhere
+import matplotlib
 matplotlib.use('Agg')
 
+import dmft.plot.hf_single_site as pss
 
 def test_example():
     """Testing a very fast single site execution of HF"""
