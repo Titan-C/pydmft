3e01af381dbfaae01204b8131e1c6d08778146a4
Óscar Nájera <najera.oscar@gmail.com>
2015-09-15 12:59:21 +0200

    matplotlib Agg for travis test

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,8 +1,14 @@
-#testing the examples before gallery
-import subprocess
+# -*- coding: utf-8 -*-
+"""testing the examples before gallery"""
+from __future__ import division, print_function, absolute_import
 import dmft.plot.hf_single_site as pss
+import matplotlib
+import subprocess
+matplotlib.use('Agg')
+
 
 def test_example():
+    """Testing a very fast single site execution of HF"""
     command = "examples/Hirsh-Fye/single_site.py -sweeps 1000 -therm 400 -Niter 6 -ofile /tmp/testhfss"
     command = command.split()
     print(subprocess.call(command))
