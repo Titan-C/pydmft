c5eb93162306a9d37fc05df3b8b2fe8edd0c554f
Óscar Nájera <najera.oscar@gmail.com>
2015-10-02 17:05:52 +0200

    Test Hirsch-Fye 2 spin flip updates

--- tests/test_qmc_hf.py
+++ tests/test_qmc_hf.py
@@ -6,14 +6,16 @@ Created on Tue Nov 25 12:44:23 2014
 """
 
 from __future__ import division, absolute_import, print_function
+from itertools import product
 import numpy as np
 import dmft.hirschfye as hf
 import pytest
 import dmft.hffast as hffast
 
 
-@pytest.mark.parametrize("chempot, u_int", [(0, 2), (0.5, 2.3)])
-def test_hf_fast_updatecond(chempot, u_int, beta=16.,
+@pytest.mark.parametrize("chempot, u_int, updater",
+                         product([0, 0.3], [2, 2.3], [hf.gnew, hffast.gnew]))
+def test_hf_fast_updatecond(chempot, u_int, updater, beta=16.,
                             n_tau=2**11, n_matsubara=64):
     parms = {'BETA': beta, 'N_TAU': n_tau, 'N_MATSUBARA': n_matsubara,
              't': 0.5, 'BANDS': 1,
@@ -29,15 +31,35 @@ def test_hf_fast_updatecond(chempot, u_int, beta=16.,
 
     g_flip = hf.gnewclean(g0ttp, v, kroneker)
     g_fast_flip = np.copy(groot)
-    hf.gnew(g_fast_flip, 2*v[flip], flip)
+    updater(g_fast_flip, 2*v[flip], flip)
 
     assert np.allclose(g_flip, g_fast_flip)
 
+
+@pytest.mark.parametrize("chempot, u_int", [(0, 2), (0.5, 2.3)])
+def test_hf_fast_2flip(chempot, u_int, beta=16.,
+                            n_tau=2**11, n_matsubara=64):
+    parms = {'BETA': beta, 'N_TAU': n_tau, 'N_MATSUBARA': n_matsubara,
+             't': 0.5, 'BANDS': 1,
+             'MU': chempot, 'U': u_int, 'dtau_mc': 0.5, 'n_tau_mc':    32, }
+    tau, w_n, g0t, __, v, __ = hf.setup_PM_sim(parms)
+    v = np.squeeze(v)
+    g0ttp = hf.retarded_weiss(g0t[:-1].reshape(-1, 1, 1))
+    kroneker = np.eye(v.size)
+
+    groot = hf.gnewclean(g0ttp, v, kroneker)
+    g_fast_flip = np.copy(groot)
     g_ffast_flip = np.copy(groot)
-    hffast.gnew(g_ffast_flip, 2*v[flip], flip)
+    for flip in [6, 10]:
+        v[flip] *= -1
+        hf.gnew(g_fast_flip, 2*v[flip], flip)
+        hffast.gnew(g_ffast_flip, 2*v[flip], flip)
+
+    g_flip = hf.gnewclean(g0ttp, v, kroneker)
+
+    assert np.allclose(g_flip, g_fast_flip)
 
     assert np.allclose(g_flip, g_ffast_flip)
-
 
 @pytest.mark.parametrize("u_int", [1, 2, 2.5])
 @pytest.mark.xfail(raises=AssertionError, reason='Atom is not well described')
