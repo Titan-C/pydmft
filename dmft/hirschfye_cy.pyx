b2ebc16790ad5d82eb17c98fac714d1556048a0c
Óscar Nájera <najera.oscar@gmail.com>
2015-10-03 21:09:11 +0200

    Runing 2flip moves. No test passing

--- dmft/hirschfye_cy.pyx
+++ dmft/hirschfye_cy.pyx
@@ -69,7 +69,8 @@ def updateDHS(np.ndarray[np.float64_t, ndim=2] gup,
               int subblock_len,
               bool Heatbath = True):
     cdef double dv, ratup, ratdw, rat
-    cdef int j, i, up, dw, pair, sn, N=v.shape[0], acc = 0, nrat = 0
+    cdef int j, i, pair, sn, N=v.shape[0], acc = 0, nrat = 0
+    cdef int jns
     sn = int(N/subblock_len)
     order = [int(a+subblock_len*b) for a in range(subblock_len) for b in range(sn)]
     for j in order:
@@ -87,4 +88,21 @@ def updateDHS(np.ndarray[np.float64_t, ndim=2] gup,
             v[j] *= -1.
             cgnew(N, &gup[0,0],  dv, j)
             cgnew(N, &gdw[0,0], -dv, j)
+        elif sn > 4:
+            jns = j+subblock_len if j<subblock_len else j-subblock_len
+            dv = -2.*v[jns]
+            ratup *= 1. + (1. - gup[jns, jns])*(exp( dv)-1.)
+            ratdw *= 1. + (1. - gdw[jns, jns])*(exp(-dv)-1.)
+            rat = ratup * ratdw
+
+            if rat<0:
+                nrat += 1
+            if Heatbath:
+                rat = rat/(1.+rat)
+
+            if rat > uniform(r):
+                acc += 1
+                v[[j, jns]] *= -1.
+                g2flip(gup,  2*v[[j, jns]], [j, jns])
+                g2flip(gdw, -2*v[[j, jns]], [j, jns])
     return acc, nrat
