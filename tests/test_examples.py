6011f18122a59dd483c1a86d431c15ba89142968
Óscar Nájera <najera.oscar@gmail.com>
2015-10-20 00:36:44 +0200

    test examples pass with new dimer tp option plot

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -24,9 +24,9 @@ def test_example(case, ofile, plot):
     command = "examples/Hirsh-Fye/{}.py -sweeps 100 -therm 400 -Niter 2 -ofile /tmp/{}".format(case, ofile)
     command = command.split()
     assert not subprocess.call(command)
-    plot.show_conv(4, 'U2.5', '/tmp/'+ofile, xlim=8)
+    plot.show_conv(4, 'U2.5', filestr='/tmp/'+ofile, xlim=8)
 
     command += '-new_seed 2.5 2.8 4'.split()
     print(command)
     assert not subprocess.call(command)
-    plot.show_conv(4, 'U2.8', '/tmp/'+ofile, xlim=8)
+    plot.show_conv(4, 'U2.8', filestr='/tmp/'+ofile, xlim=8)
