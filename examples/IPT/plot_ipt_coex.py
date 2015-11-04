7f4d2e2e20b6ce7ca0ad73e941e34548dac55691
Óscar Nájera <najera.oscar@gmail.com>
2015-11-04 22:09:48 +0100

    Single band IPT now calculates double occupation

--- examples/IPT/plot_ipt_coex.py
+++ examples/IPT/plot_ipt_coex.py
@@ -18,16 +18,19 @@ from dmft.twosite import matsubara_Z
 import numpy as np
 import matplotlib.pylab as plt
 
-U = np.linspace(0, 3.8, 41)
+U = np.linspace(0.1, 3.8, 41)
 U = np.concatenate((U, np.linspace(3.6, 2.4, 16)-0.05))
 
 parms = {'MU': 0, 't': 0.5, 'N_MATSUBARA': 256}
 lop_g=[]
 figz, axz = plt.subplots()
 figf, axf = plt.subplots()
+figd, axd = plt.subplots()
 for beta in [16, 25, 50, 100, 200]:
     u_zet = []
     u_fl = []
+    g_log = []
+    sigma_log = []
     parms['BETA'] = beta
     tau, w_n = tau_wn_setup(parms)
     g_iwn = greenF(w_n, D=2*parms['t'])
@@ -36,9 +39,20 @@ for beta in [16, 25, 50, 100, 200]:
         lop_g.append(g_iwn)
         u_zet.append(matsubara_Z(sigma.imag, beta))
         u_fl.append(-fit_gf(w_n[:3], g_iwn.imag)(0.))
+        g_log.append(g_iwn)
+        sigma_log.append(sigma)
 
     axz.plot(U, u_zet, '+-', label='$\\beta={}$'.format(beta))
     axf.plot(U, u_fl, 'x:', label='$\\beta={}$'.format(beta))
+    # Double occupation
+    D=[2/u*(2*(0.5*s*g+u**2/8./w_n**2).real.sum()/beta -0.25*beta*u**2/8.) for g, s, u in zip(g_log, sigma_log, U)]
+    axd.plot(U, np.asarray(D)+0.25, '-', label='$\\beta={}$'.format(beta))
+    ## Energies
+    #T=[1j*w_n*(g[i]-gfree_iwn)-s[i]*g[i] for i in range(len(g))]
+    #Tn=np.asarray([2*a.sum() for a in T]).real/200. +emean
+    #H=[1j*w_n*(g[i]-gfree_iwn)-0.5*s[i]*g[i] for i in range(len(g))]
+    #Hn=np.asarray([2*a.sum() for a in H]).real/200.+emean
+
 axz.set_title('Hysteresis loop of the quasiparticle weigth')
 axz.legend(loc=0)
 axz.set_ylabel('Z')
@@ -46,3 +60,7 @@ axz.set_xlabel('U/D')
 axz.set_title('Hysteresis loop of the Density of states')
 axf.set_ylabel('Dos(0)')
 axf.set_xlabel('U/D')
+axd.set_title('Hysteresis loop of the double occupation')
+axd.legend(loc=0)
+axd.set_ylabel(r'$\langle d \rangle$')
+axd.set_xlabel('U/D')
