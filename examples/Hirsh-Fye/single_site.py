27043880575a199e3006cfbb8fb7f31cb46744aa
Óscar Nájera <najera.oscar@gmail.com>
2015-11-13 15:54:53 +0100

    Single band script for Hirsch-Fye AFM

--- examples/Hirsh-Fye/single_site.py
+++ examples/Hirsh-Fye/single_site.py
@@ -36,6 +36,8 @@ def dmft_loop_pm(simulation):
     setup.update(simulation)
 
     tau, w_n, _, giw, v_aux, intm = hf.setup_PM_sim(setup)
+    if setup['AFM']:
+        giw = giw + np.array([[-1], [1]])*1e-2/w_n**2
 
     try:
         with h5.File(setup['ofile'].format(**setup), 'a') as store:
@@ -53,15 +55,26 @@ def dmft_loop_pm(simulation):
         if comm.rank == 0:
             print('On loop', iter_count, 'beta', setup['BETA'], 'U', setup['U'])
 
-        # patch tail on
-        giw.real = 0.
 
-        g0iw = 1/(1j*w_n + setup['MU'] - setup['t']**2 * giw)
-        g0tau = gf.gw_invfouriertrans(g0iw, tau, w_n)
-        gtu, gtd = hf.imp_solver([g0tau]*2, v_aux, intm, setup)
-        gtau = -np.squeeze(0.5 * (gtu+gtd))
+        if setup['AFM']:
+            g0iw = 1/(1j*w_n + setup['MU'] - setup['t']**2 * giw[[1, 0]])
+            g0tau = gf.gw_invfouriertrans(g0iw, tau, w_n)
+            gtu, gtd = hf.imp_solver([g0tau[0], g0tau[1]], v_aux, intm, setup)
+            gtau = -np.squeeze([gtu, gtd])
 
-        giw = gf.gt_fouriertrans(gtau, tau, w_n)
+            giw = gf.gt_fouriertrans(gtau, tau, w_n,
+                                    [1., -setup['U']*(0.5+gtau[:, 0]).reshape(2, 1),
+                                                    (setup['U']/2)**2])
+        else:
+            # enforce Half-fill, particle-hole symmetry
+            giw.real = 0.
+
+            g0iw = 1/(1j*w_n + setup['MU'] - setup['t']**2 * giw)
+            g0tau = gf.gw_invfouriertrans(g0iw, tau, w_n)
+            gtu, gtd = hf.imp_solver([g0tau]*2, v_aux, intm, setup)
+            gtau = -np.squeeze(0.5 * (gtu+gtd))
+
+            giw = gf.gt_fouriertrans(gtau, tau, w_n, [1., 0., setup['U']**2/4])
 
         if comm.rank == 0:
             with h5.File(setup['ofile'].format(**setup), 'a') as store:
@@ -73,6 +86,8 @@ def dmft_loop_pm(simulation):
 if __name__ == "__main__":
 
     SETUP = hf.do_input('DMFT Loop For the single band para-magnetic case')
+    SETUP.add_argument('-afm', '--AFM', action='store_true',
+                        help='Use the self-consistency for Antiferromagnetism')
     SETUP = vars(SETUP.parse_args())
 
     dmft_loop_pm(SETUP)
