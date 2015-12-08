Fix Potential energy calculation in Hf dimer

dmft/plot/hf_dimer.py
@@ -231,20 +231,22 @@ def ekin(BETA, u_str, tp=0.25, filestr='tp{tp}_B{BETA}.h5',):
     return T
 
 
-def epot(BETA, u_str, tp=0.25, filestr='tp{tp}_B{BETA}.h5',):
-    with h5.File(filestr.format(BETA), 'r') as results:
-        last_iter = results[u_str].keys()[-1]
-        setup = h5.get_attributes(results[u_str][last_iter])
-        tau, w_n = gf.tau_wn_setup(setup)
-        wsqr_4 = 4*w_n*w_n
-        giwd, giwo = get_giw(results[u_str], last_iter, tau, w_n)
-        siwd, siwo = get_giw(results[u_str], last_iter, tau, w_n)
+def epot(BETA, tp=0.25, filestr='tp{tp}_B{BETA}.h5',):
+    tau, w_n = gf.tau_wn_setup(dict(BETA=BETA, N_MATSUBARA=BETA))
+    wsqr_4 = 4*w_n*w_n
+    V = []
+    with h5.File(filestr.format(tp=tp, BETA=BETA), 'r') as results:
+        for u_str in results:
+            last_iter = results[u_str].keys()[-1]
+            giwd, giwo = get_giw(results[u_str], last_iter, tau, w_n)
+            siwd, siwo = get_sigmaiw(results[u_str], last_iter, tau, w_n)
 
-        u_int = float(u_str[1:])
+            u_int = float(u_str[1:])
 
-        V = (giwo*siwo + giwd*siwd + u_int**2/wsqr_4).sum()/BETA
+            V.append((giwo.real*siwo.real - giwd.imag*siwd.imag + u_int**2/wsqr_4).sum()/BETA)
+        ur = np.array([float(u_str[1:]) for u_str in results])
 
-    return V - BETA*u_int**2/32
+    return np.array(V) - BETA*ur**2/32, ur
 
 def docc_plot(BETA, tp, filestr, ax=None):
     """Plots double occupation"""
@@ -260,9 +262,10 @@ def docc_plot(BETA, tp, filestr, ax=None):
             except KeyError:
                 docc = np.nan
 
-        docc = np.array(docc).T
+        docc=np.array(docc).T
         ax.scatter(docc[0], docc[1], c=docc[1],
                     s=120, marker='<', vmin=0, vmax=0.2)
+        ax.plot(docc[0], docc[1], ':')
     ax.set_title(r'double occupation @'
                  r'$\beta={}$, tp={}'.format(BETA, tp))
     ax.set_ylabel(r'$\langle n_\uparrow n_\downarrow \rangle$')