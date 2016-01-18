96be7d47279037a6ee63b67412e618f8ca8919df
Óscar Nájera <najera.oscar@gmail.com>
2016-01-18 10:27:55 +0100

    Save JSON setup on h5-npy conversion

--- dmft/h5archive.py
+++ dmft/h5archive.py
@@ -7,6 +7,7 @@ H5PY interface
 # Author: Óscar Nájera
 
 from __future__ import division, absolute_import, print_function
+import json
 import os
 from h5py import File
 import numpy as np
@@ -41,6 +42,12 @@ def _make_npy(name, obj):
     except AttributeError:
         if not os.path.exists(name):
             os.makedirs(name)
+        setup = get_attributes(obj)
+        setup = {str(a): v for a, v in setup.items() if not isinstance(v, np.bool_)}
+        if setup:
+            with open(os.path.dirname(name) + '/setup', 'w') as fname:
+                json.dump(setup, fname, indent=2)
+
 
 def h5_2_npy(h5file):
     """Converts a h5 data file into a folder tree with npy files"""
@@ -48,7 +55,9 @@ def h5_2_npy(h5file):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
     cwd = os.getcwd()
-    with File(h5file, 'r') as source:
-        os.chdir(target_dir)
-        source.visititems(_make_npy)
-    os.chdir(cwd)
+    try:
+        with File(h5file, 'r') as source:
+            os.chdir(target_dir)
+            source.visititems(_make_npy)
+    finally:
+        os.chdir(cwd)
