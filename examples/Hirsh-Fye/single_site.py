52e2e5fcf771f239643f5267f17b770e075fd21f
Óscar Nájera <najera.oscar@gmail.com>
2016-01-20 14:17:27 +0100

    AFM self-consistency in HF-SB
    After a long study of the simulation behavior. I choose to follow the
    AFM solution by spin flipping the bath generator, so to have stable
    shaped output an simplify the convergence tests. Also each new iteration
    is continued from the last configuration space this shortens the
    thermalization time making easier to accumulate good measurements

--- examples/Hirsh-Fye/single_site.py
+++ examples/Hirsh-Fye/single_site.py
@@ -65,12 +65,10 @@ def dmft_loop_pm(simulation, U, g_iw_start=None):
 
         if COMM.rank == 0:
             print('On loop', iter_count, 'beta', setup['BETA'], 'U', setup['U'])
-            if not os.path.exists(work_dir):
-                os.makedirs(work_dir)
 
 
         if setup['AFM']:
-            g0iw = 1/(1j*w_n + setup['MU'] - setup['t']**2 * giw)
+            g0iw = 1/(1j*w_n + setup['MU'] - setup['t']**2 * giw[[1, 0]])
             g0tau = gf.gw_invfouriertrans(g0iw, tau, w_n, [1., 0., .25])
             gtu, gtd = hf.imp_solver([g0tau[0], g0tau[1]], v_aux, intm, setup)
             gtau = -np.squeeze([gtu, gtd])
