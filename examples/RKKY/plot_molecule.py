fbb94d01e7d006362f6d897da39f176274b2feac
Óscar Nájera <najera.oscar@gmail.com>
2016-02-10 02:23:37 +0100

    Ploting the eigenspectra of molecule

--- examples/RKKY/plot_molecule.py
+++ examples/RKKY/plot_molecule.py
@@ -10,15 +10,19 @@ spectral function are evaluated by means of the Lehmann representation
 # author: Óscar Nájera
 
 from __future__ import division, absolute_import, print_function
-from dmft.common import matsubara_freq, gw_invfouriertrans
-import dmft.RKKY_dimer as rt
 from itertools import product
-import slaveparticles.quantum.operators as op
 import matplotlib.pyplot as plt
 import numpy as np
+from dmft.common import matsubara_freq, gw_invfouriertrans
+import dmft.RKKY_dimer as rt
+import slaveparticles.quantum.operators as op
+
+###############################################################################
+# Real space representation
+# =========================
 
 
-def plot_real_gf(eig_e, eig_v, oper_pair, c_v, names):
+def plot_real_gf(eig_e, eig_v, oper_pair, c_v, names, beta):
     _, axw = plt.subplots(2, sharex=True)
     w = np.linspace(-1.5, 1.5, 500) + 1j*1e-2
     gfs = [op.gf_lehmann(eig_e, eig_v, c.T, beta, w, d) for c, d in oper_pair]
@@ -28,11 +32,11 @@ def plot_real_gf(eig_e, eig_v, oper_pair, c_v, names):
         axw[0].legend()
         axw[0].set_title(r'Real Frequencies Green functions, $\beta={}$'.format(beta))
         axw[0].set_ylabel(r'$\Re e G(\omega)$')
-        axw[1].set_ylabel(r'$A(\omega)$')
+        axw[1].set_ylabel(r'$-\Im m(\omega)/\pi$')
         axw[1].set_xlabel(r'$\omega$')
 
 
-def plot_matsubara_gf(eig_e, eig_v, oper_pair, c_v, names):
+def plot_matsubara_gf(eig_e, eig_v, oper_pair, c_v, names, beta, U, mu, tp):
     gwp, axwn = plt.subplots(2, sharex=True)
     gwp.subplots_adjust(hspace=0)
     gtp, axt = plt.subplots()
@@ -43,7 +47,7 @@ def plot_matsubara_gf(eig_e, eig_v, oper_pair, c_v, names):
         axwn[0].plot(wn, giw.real, color+'s-', label=r'${}$'.format(name))
         axwn[1].plot(wn, giw.imag, color+'o-')
 
-        tail =  [0., tp, 0.] if name[0]!=name[1] else [1., 0., 0.]
+        tail =  [0., tp, 0.] if name[0]!=name[1] else [1., 0., U**2/4.]
         gt = gw_invfouriertrans(giw, tau, wn, tail)
         axt.plot(tau, gt, label=r'${}$'.format(name))
 
@@ -59,31 +63,68 @@ def plot_matsubara_gf(eig_e, eig_v, oper_pair, c_v, names):
         axt.set_xlabel(r'$\tau$')
         axt.set_ylabel(r'$G(\tau)$')
 
+def plot_eigen_spectra(U, mu, tp):
+    plt.figure()
+    plt.title('Many particle Energy Spectra U={} $t_\perp={}$'.format(U, tp))
+    h_at, oper = rt.dimer_hamiltonian(U, mu, tp)
+    eig_e = []
+    eig_e.append(LA.eigvalsh(h_at[1:5, 1:5].todense()))
+    eig_e.append(LA.eigvalsh(h_at[5:11, 5:11].todense()))
+    eig_e.append(LA.eigvalsh(h_at[11:15,11:15].todense()))
 
-beta = 50
-U = 1.
-mu = 0.
-tp = 0.25
-c_v = ['b', 'g', 'r', 'k']
-names = [r'a\uparrow', r'a\downarrow', r'b\uparrow', r'b\downarrow']
+    plt.plot(np.concatenate(eig_e), label=str(t))
+    plt.legend()
 
-h_at, oper = rt.dimer_hamiltonian(U, mu, tp)
-eig_e, eig_v = op.diagonalize(h_at.todense())
-oper_pair = list(product([oper[0], oper[2]], repeat=2))
-names = list(product('AB', repeat=2))
-plot_real_gf(eig_e, eig_v, oper_pair, c_v, names)
-plot_matsubara_gf(eig_e, eig_v, oper_pair, c_v, names)
+    plt.axvline(x=3.5)
+    plt.axvline(x=11.5)
+
+def plot_greenfunctions(beta, U, mu, tp):
+    c_v = ['b', 'g', 'r', 'k']
+    names = [r'a\uparrow', r'a\downarrow', r'b\uparrow', r'b\downarrow']
+    plot_eigen_spectra(U, mu, tp)
+
+    h_at, oper = rt.dimer_hamiltonian(U, mu, tp)
+    oper_pair = list(product([oper[0], oper[2]], repeat=2))
+    names = list(product('AB', repeat=2))
+    plot_real_gf(eig_e, eig_v, oper_pair, c_v, names, beta)
+    plot_matsubara_gf(eig_e, eig_v, oper_pair, c_v, names, beta, U, mu, tp)
 
 
+###############################################################################
+# The non-interacting case $t_\perp=0.2$
+# --------------------------------------
+
+plot_greenfunctions(50, 0, 0, 0.2)
+
+###############################################################################
+# The non-interacting case $t_\perp=0.8$
+# --------------------------------------
+
+plot_greenfunctions(50, 0, 0, 0.8)
+
+
+###############################################################################
+# The interacting case $U=1$ $t_\perp=0.2$
+# --------------------------------------
+
+plot_greenfunctions(50, 1, 0, 0.2)
+###############################################################################
+# The interacting case $U=1$ $t_\perp=0.8$
+# --------------------------------------
+
+plot_greenfunctions(50, 1, 0, 0.5)
+eig_e.append(LA.eigvalsh(h_at[1:5, 1:5].todense()))
+eig_e.append(LA.eigvalsh(h_at[5:11, 5:11].todense()))
+eig_e.append(LA.eigvalsh(h_at[11:15,11:15].todense()))
 ############################################################
 # The symmetric and anti-symmetric bands
 # ======================================
 #
 
-h_at, oper = rt.dimer_hamiltonian_bond(U, mu, tp)
-eig_e, eig_v = op.diagonalize(h_at.todense())
-
-oper_pair = product([oper[0], oper[2]], repeat=2)
+#h_at, oper = rt.dimer_hamiltonian_bond(U, mu, tp)
+#eig_e, eig_v = op.diagonalize(h_at.todense())
+#
+#oper_pair = product([oper[0], oper[2]], repeat=2)
 
 #plot_real_gf(eig_e, eig_v, oper_pair, c_v, names)
 
