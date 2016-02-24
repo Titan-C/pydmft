5acb4b9dcc901fd66229c3f69ff27a978c51070d
Óscar Nájera <najera.oscar@gmail.com>
2016-02-24 18:56:27 +0100

    Phases Energy change

--- examples/RKKY/plot_phase_diagram.py
+++ examples/RKKY/plot_phase_diagram.py
@@ -49,3 +49,44 @@ ax[0].plot(3.5*np.ones_like(temp), temp, 'rx-', lw=2)
 ax[0].plot(4.3*np.ones_like(temp), temp, 'rx-', lw=2)
 
 ax[1].plot([.2]*4, [2, 2.65, 3.5, 4.3], 'ro', ms=10)
+
+
+import dmft.common as gf
+import dmft.ipt_imag as ipt
+def loop_beta(u_int, tp, betarange, seed='mott gap'):
+    giw_s = []
+    sigma_iw = []
+    ekin, epot = [], []
+    iterations = []
+    for beta in betarange:
+        tau, w_n = gf.tau_wn_setup(dict(BETA=beta, N_MATSUBARA=max(5*beta, 256)))
+        giw_d, giw_o = rt.gf_met(w_n, 0., 0., 0.5, 0.)
+        if seed == 'mott gap':
+            giw_d, giw_o = 1/(1j*w_n - 4j/w_n), np.zeros_like(w_n)+0j
+
+        giw_d, giw_o, loops = rt.ipt_dmft_loop(beta, u_int, -tp, giw_d, giw_o)
+        giw_s.append((giw_d, giw_o))
+        iterations.append(loops)
+        g0iw_d, g0iw_o = rt.self_consistency(1j*w_n, 1j*giw_d.imag, giw_o.real, 0., -tp, 0.25)
+        siw_d, siw_o = ipt.dimer_sigma(u_int, -tp, g0iw_d, g0iw_o, tau, w_n)
+        sigma_iw.append((siw_d.copy(), siw_o.copy()))
+
+        ekin.append(rt.ekin(giw_d, giw_o, w_n, beta))
+
+        epot.append(rt.epot(giw_d, giw_o, tau, w_n, -tp, u_int, beta))
+
+    print(np.array(iterations))
+
+    return giw_s, sigma_iw, np.array(ekin), np.array(epot), w_n
+
+giw_s, sigma_iw, ekin, epot, w_n = loop_beta(4, .50, BETARANGE, seed='omott gap')
+
+plt.figure(1)
+plt.plot(1/BETARANGE, ekin+epot, '+-')
+#plt.plot(1/BETARANGE, epot, '+-')
+
+H=ekin+epot
+CV=(H[1:]-H[:-1])/(temp[1:]-temp[:-1])
+
+plt.figure(2)
+plt.plot(temp[:-1],CV, '+-')
