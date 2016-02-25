cd8411e78b806852fdee6cbce585a2b3f4e9ec61
Óscar Nájera <najera.oscar@gmail.com>
2016-02-25 15:17:35 +0100

    IPT dimer rerun on npy files save sigma and Energy

--- examples/RKKY/ipt_dimer.py
+++ examples/RKKY/ipt_dimer.py
@@ -10,60 +10,74 @@ Simple IPT solver for a dimer
 # Author: Óscar Nájera
 
 from __future__ import division, absolute_import, print_function
+import itertools
+import json
+import os
+import numpy as np
 from joblib import Parallel, delayed
-import argparse
 import dmft.RKKY_dimer as rt
 import dmft.common as gf
-import dmft.h5archive as h5
 import dmft.ipt_imag as ipt
-import numpy as np
 
 
-def loop_tp_u(urange, tprange, BETA, filestr, metal=True):
-    tau, w_n = gf.tau_wn_setup(dict(BETA=BETA, N_MATSUBARA=max(5*BETA, 256)))
-    for tp in tprange:
-        giw_d, giw_o = rt.gf_met(w_n, 0., tp, 0.5, 0.)
-        if not metal:
-            giw_d = 1/(1j*w_n - 4j/w_n)
-            giw_o[:] = 0.
+def loop_tp_u(tprange, u_range, beta, filestr, seed='mott gap'):
 
-        for u_int in urange:
-            giw_d, giw_o, loops = rt.ipt_dmft_loop(BETA, u_int, tp, giw_d, giw_o)
+    save_dir = filestr.format(beta)
+    if np.allclose(tprange, tprange[0]):
+        save_dir = os.path.join(save_dir, 'tp'+str(tprange[0]))
+    elif np.allclose(u_range, u_range[0]):
+        save_dir = os.path.join(save_dir, 'U'+str(u_range[0]))
 
-            with h5.File(filestr.format(BETA), 'a') as store:
-                u_group = '/tp{}/U{}/'.format(tp, u_int)
-                store[u_group+'giw_d'] = giw_d.imag
-                store[u_group+'giw_o'] = giw_o.real
-                store[u_group+'loops'] = loops
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    setup = {'beta': beta, 'tprange': tprange.tolist(), 'u_range': u_range.tolist()}
+    with open(save_dir + '/setup', 'w') as conf:
+        json.dump(setup, conf, indent=2)
+
+###############################################################################
+
+    tau, w_n = gf.tau_wn_setup(dict(BETA=beta, N_MATSUBARA=max(5*beta, 256)))
+    giw_d, giw_o = rt.gf_met(w_n, 0., tprange[0], 0.5, 0.)
+    if seed == 'mott gap':
+        giw_d, giw_o = 1/(1j*w_n + 4j/w_n), np.zeros_like(w_n)+0j
+
+    giw_s = []
+    sigma_iw = []
+    ekin, epot = [], []
+    iterations = []
+    for tp, u_int in zip(tprange, u_range):
+        giw_d, giw_o, loops = rt.ipt_dmft_loop(beta, u_int, tp, giw_d, giw_o, 1e-5)
+        giw_s.append((giw_d, giw_o))
+        iterations.append(loops)
+        g0iw_d, g0iw_o = rt.self_consistency(1j*w_n, 1j*giw_d.imag, giw_o.real, 0., tp, 0.25)
+        siw_d, siw_o = ipt.dimer_sigma(u_int, tp, g0iw_d, g0iw_o, tau, w_n)
+        sigma_iw.append((siw_d.copy(), siw_o.copy()))
+
+        ekin.append(rt.ekin(giw_d, giw_o, w_n, beta))
+
+        epot.append(rt.epot(giw_d, giw_o, tau, w_n, tp, u_int, beta))
 
 
-def loop_u_tp(urange, tprange, BETA, filestr, metal=True):
-    tau, w_n = gf.tau_wn_setup(dict(BETA=BETA, N_MATSUBARA=max(5*BETA, 256)))
-    for u_int in urange:
-        giw_d, giw_o = rt.gf_met(w_n, 0., tprange[0], 0.5, 0.)
-        if not metal:
-            giw_d = 1/(1j*w_n - 4j/w_n)
-            giw_o[:] = 0.
+    np.save(save_dir + '/giw', np.array(giw_s))
+    np.save(save_dir + '/sigmaiw', np.array(sigma_iw))
+    np.save(save_dir + '/ekin', np.array(ekin))
+    np.save(save_dir + '/epot', np.array(epot))
+    np.save(save_dir + '/complexity', np.array(iterations))
 
-        for tp in tprange:
-            giw_d, giw_o, loops = rt.ipt_dmft_loop(BETA, u_int, tp, giw_d, giw_o)
 
-            with h5.File(filestr.format(BETA), 'a') as store:
-                u_group = '/tp{}/U{}/'.format(tp, u_int)
-                store[u_group+'giw_d'] = giw_d.imag
-                store[u_group+'giw_o'] = giw_o.real
-                store[u_group+'loops'] = loops
 
 if __name__ == "__main__":
 
     tpr = np.hstack((np.arange(0, 0.5, 0.02), np.arange(0.5, 1.1, 0.05)))
     ur = np.arange(0, 4.5, 0.1)
-    BETARANGE = np.round(np.hstack(([768, 512], np.logspace(8, -4.5, 41, base=2))), 2)
+    BETARANGE = np.round(np.hstack(([756., 512.], np.logspace(8, -4.5, 45, base=2), [.03, .02])), 2)
+    jobs = [(job.T[0], job.T[1], BETA, 'disk/Dimer_ipt_B{}', 'metal')
+            for BETA in BETARANGE
+            for job in np.array(list(itertools.product(tpr, ur))).reshape(len(tpr), len(ur), 2)]
+    jobs+= [(job.T[0], job.T[1][::-1], BETA, 'disk/Dimer_ins_ipt_B{}')
+            for BETA in BETARANGE
+            for job in np.array(list(itertools.product(tpr, ur))).reshape(len(tpr), len(ur), 2)]
 
-    # jobs = [(ur, tpr, BETA, 'disk/Dimer_ipt_B{}.h5') for BETA in BETARANGE]
-    # jobs+= [(ur[::-1], tpr, BETA, 'disk/Dimer_ins_ipt_B{}.h5', False) for BETA in BETARANGE]
-#
-    # Parallel(n_jobs=-1, verbose=5)(delayed(loop_tp_u)(*job) for job in jobs)
 
-    jobs = [(ur, tpr[::-1], BETA, 'disk/Dimer_rtp_ipt_B{}.h5') for BETA in BETARANGE]
-    Parallel(n_jobs=-1, verbose=5)(delayed(loop_u_tp)(*job) for job in jobs)
+    Parallel(n_jobs=-1, verbose=5)(delayed(loop_tp_u)(*job) for job in jobs)
