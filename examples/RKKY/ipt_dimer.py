Kinetic Energy fix tail signs
Update definition

Update potential energy calls with sigma and not tau

examples/RKKY/ipt_dimer.py
@@ -59,7 +59,7 @@ def loop_tp_u(tprange, u_range, beta, filestr, seed='mott gap'):
 
         ekin.append(rt.ekin(giw_d, giw_o, w_n, tp, beta))
 
-        epot.append(rt.epot(giw_d, giw_o, tau, w_n, tp, u_int, beta))
+        epot.append(rt.epot(giw_d, giw_o, siw_d, siw_o, w_n, tp, u_int, beta))
 
     np.save(save_dir + '/giw', np.array(giw_s))
     np.save(save_dir + '/sigmaiw', np.array(sigma_iw))

examples/RKKY/plot_dimer_insulator_study.py
@@ -38,7 +38,7 @@ def loop_u_tp(u_range, tprange, beta, seed='mott gap'):
 
         ekin.append(rt.ekin(giw_d, giw_o, w_n, tp, beta))
 
-        epot.append(rt.epot(giw_d, giw_o, tau, w_n, tp, u_int, beta))
+        epot.append(rt.epot(giw_d, giw_o, siw_d, siw_o, w_n, tp, u_int, beta))
 
     print(np.array(iterations))
 