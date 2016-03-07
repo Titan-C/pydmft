55ed3db5ec377792755a1c87e713c306140fcc6e
Óscar Nájera <najera.oscar@gmail.com>
2016-03-07 03:20:52 +0100

    Scattering rates on fix beta

--- examples/RKKY/plot_scattering.py
+++ examples/RKKY/plot_scattering.py
@@ -10,6 +10,7 @@ Scattering rates
 
 from __future__ import division, absolute_import, print_function
 
+import itertools
 from math import log, ceil
 import numpy as np
 import matplotlib.pyplot as plt
@@ -96,8 +97,8 @@ si[0].legend(loc=0, prop={'size': 8})
 
 si[0].set_ylabel(r'-$\Im \Sigma_{AA}(w=0)$')
 ds[0].set_ylabel(r'-$\Im d\Sigma_{AA}(w=0)$')
-si[1].set_ylabel(r'-$\Im \Sigma_{AB}(w=0)$')
-ds[1].set_ylabel(r'-$\Im d\Sigma_{AB}(w=0)$')
+si[1].set_ylabel(r'-$\Re \Sigma_{AB}(w=0)$')
+ds[1].set_ylabel(r'-$\Re d\Sigma_{AB}(w=0)$')
 ds[0].set_xlim([0, .06])
 
 ds[0].set_xlabel('$T/D$')
@@ -121,10 +122,108 @@ plot_zero_w(sigmasM_tp, tp_r, BETARANGE, 1, 'MET tp=', si[1], ds[1])
 
 si[0].set_ylabel(r'-$\Im \Sigma_{AA}(w=0)$')
 ds[0].set_ylabel(r'-$\Im d\Sigma_{AA}(w=0)$')
-si[1].set_ylabel(r'-$\Im \Sigma_{AB}(w=0)$')
-ds[1].set_ylabel(r'-$\Im d\Sigma_{AB}(w=0)$')
+si[1].set_ylabel(r'-$\Re \Sigma_{AB}(w=0)$')
+ds[1].set_ylabel(r'-$\Re d\Sigma_{AB}(w=0)$')
 ds[0].set_xlim([0, .06])
 
 si[0].legend(loc=0, prop={'size': 8})
 ds[0].set_xlabel('$T/D$')
 ds[1].set_xlabel('$T/D$')
+
+
+###############################################################################
+# Fine graphics
+f, (si, ds) = plt.subplots(2, 1, sharex=True)
+plot_zero_w([sigmasI_U[1]], [3.], BETARANGE, 0, 'INS', si, ds)
+plot_zero_w([sigmasM_U[1]], [3.], BETARANGE, 0, 'MET', si, ds)
+si.set_title(r'$\Im m\Sigma_{Aa}$ cut and slope tp=0.3, U=3')
+si.set_ylabel(r'-$\Im \Sigma_{AA}(w=0)$')
+ds.set_ylabel(r'-$\Im d\Sigma_{AA}(w=0)$')
+ds.set_xlim([0, .06])
+si.legend(loc=0, prop={'size': 10})
+ds.set_xlabel('$T/D$')
+
+f, (si, ds) = plt.subplots(2, 1, sharex=True)
+plot_zero_w([sigmasI_U[1]], [3.], BETARANGE, 1, 'INS', si, ds)
+plot_zero_w([sigmasM_U[1]], [3.], BETARANGE, 1, 'MET', si, ds)
+si.set_title(r'$\Re e\Sigma_{AB}$ cut and slope tp=0.3, U=3')
+si.set_ylabel(r'-$\Re e \Sigma_{AB}(w=0)$')
+ds.set_ylabel(r'-$\Re e d\Sigma_{AB}(w=0)$')
+ds.set_xlim([0, .06])
+
+si.legend(loc=0, prop={'size': 10})
+ds.set_xlabel('$T/D$')
+
+
+###############################################################################
+# Study a fix Beta
+
+def loop_u_tp(u_range, tp_range, beta, seed='mott gap'):
+    """Solves IPT dimer and return Im Sigma_AA, Re Simga_AB
+
+    returns list len(betarange) x 2 Sigma arrays
+"""
+    tau, w_n = gf.tau_wn_setup(
+        dict(BETA=beta, N_MATSUBARA=max(2**ceil(log(4 * beta) / log(2)), 256)))
+    giw_d, giw_o = rt.gf_met(w_n, 0., 0., 0.5, 0.)
+    if seed == 'I':
+        giw_d, giw_o = 1 / (1j * w_n - 4j / w_n), np.zeros_like(w_n) + 0j
+
+    sigma_iw = []
+    iterations = []
+    for tp, u_int in zip(tp_range, u_range):
+        giw_d, giw_o, loops = rt.ipt_dmft_loop(
+            beta, u_int, tp, giw_d, giw_o, tau, w_n, 1e-8)
+        iterations.append(loops)
+        g0iw_d, g0iw_o = rt.self_consistency(
+            1j * w_n, 1j * giw_d.imag, giw_o.real, 0., tp, 0.25)
+        siw_d, siw_o = ipt.dimer_sigma(u_int, tp, g0iw_d, g0iw_o, tau, w_n)
+        sigma_iw.append((siw_d.imag, siw_o.real))
+
+    print(np.array(iterations))
+
+    return sigma_iw
+
+###############################################################################
+
+
+def plot_zero_w(function_array, iter_range, beta, entry, label_head, ax, dx):
+    """Plot the zero frequency extrapolation of a function
+    Parameters
+    ----------
+    function_array: real ndarray
+      contains the function (G, Sigma) to linearly fit over 2 first frequencies
+    iter_range: list floats
+      values of changing variable U or tp
+    berarange: real ndarray 1D, values of beta
+    entry: 0, 1 corresponds to diagonal or off-diagonal entry of function
+    label_head: string for label
+    ax, dx: matplotlib axis to plot in
+    """
+    tau, w_n = gf.tau_wn_setup(dict(BETA=beta, N_MATSUBARA=20))
+
+    dat = []
+    for j, u in enumerate(iter_range):
+        dat.append(np.polyfit(w_n[:2], function_array[j][entry][:2], 1))
+    ax.plot(iter_range, -np.array(dat)[:, 1], label=label_head)
+    dx.plot(iter_range, -np.array(dat)[:, 0], label=label_head)
+
+U_int = np.arange(.5, 4.5, 0.1)
+sigmasM_Ur = loop_u_tp(U_int, .3 * np.ones_like(U_int), 200., 'M')
+sigmasI_Ur = loop_u_tp(U_int[::-1], .3 * np.ones_like(U_int), 200., 'I')[::-1]
+
+f, (si, ds) = plt.subplots(2, 2, sharex=True)
+plot_zero_w(sigmasI_Ur, U_int, 100., 0, 'INS', si[0], ds[0])
+plot_zero_w(sigmasM_Ur, U_int, 100., 0, 'MET', si[0], ds[0])
+plot_zero_w(sigmasI_Ur, U_int, 100., 1, 'INS', si[1], ds[1])
+plot_zero_w(sigmasM_Ur, U_int, 100., 1, 'MET', si[1], ds[1])
+
+si[0].legend(loc=0, prop={'size': 8})
+
+si[0].set_ylabel(r'-$\Im \Sigma_{AA}(w=0)$')
+ds[0].set_ylabel(r'-$\Im d\Sigma_{AA}(w=0)$')
+si[1].set_ylabel(r'-$\Re \Sigma_{AB}(w=0)$')
+ds[1].set_ylabel(r'-$\Re d\Sigma_{AB}(w=0)$')
+
+ds[0].set_xlabel('$U/D$')
+ds[1].set_xlabel('$U/D$')
