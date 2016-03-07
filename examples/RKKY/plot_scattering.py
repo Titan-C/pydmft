c08c7e897983016776c42d2ae10f9fa37530d74c
Óscar Nájera <najera.oscar@gmail.com>
2016-03-07 02:21:17 +0100

    Scattering plots better defined

--- examples/RKKY/plot_scattering.py
+++ examples/RKKY/plot_scattering.py
@@ -12,16 +12,19 @@ from __future__ import division, absolute_import, print_function
 
 from math import log, ceil
 import numpy as np
-from scipy.integrate import simps
 import matplotlib.pyplot as plt
 from joblib import Parallel, delayed
-from joblib import Memory
 import dmft.RKKY_dimer as rt
 import dmft.common as gf
 import dmft.ipt_imag as ipt
 
 
 def loop_beta(u_int, tp, betarange, seed='mott gap'):
+    """Solves IPT dimer and return Im Sigma_AA, Re Simga_AB
+
+    returns list len(betarange) x 2 Sigma arrays
+"""
+
     sigma_iw = []
     iterations = []
     for beta in betarange:
@@ -37,13 +40,13 @@ def loop_beta(u_int, tp, betarange, seed='mott gap'):
         g0iw_d, g0iw_o = rt.self_consistency(
             1j * w_n, 1j * giw_d.imag, giw_o.real, 0., tp, 0.25)
         siw_d, siw_o = ipt.dimer_sigma(u_int, tp, g0iw_d, g0iw_o, tau, w_n)
-        sigma_iw.append((siw_d, siw_o))
+        sigma_iw.append((siw_d.imag, siw_o.real))
 
     print(np.array(iterations))
 
     return sigma_iw
 
-temp = np.arange(1 / 500., .4, 1 / 100)
+temp = np.arange(1 / 500., .061, 1 / 250)
 BETARANGE = 1 / temp
 
 ###############################################################################
@@ -59,24 +62,46 @@ sigmasI_U = Parallel(n_jobs=-1)(delayed(loop_beta)(u_int, .3, BETARANGE, 'I')
 # Plot a 2x2 frame following sigma and d_sigma Changing U
 
 
-def plot_low_w(function_array, iter_range, betarange, label_head, ax, dx):
+def plot_zero_w(function_array, iter_range, betarange, entry, label_head, ax, dx):
+    """Plot the zero frequency extrapolation of a function
+    Parameters
+    ----------
+    function_array: real ndarray
+      contains the function (G, Sigma) to linearly fit over 2 first frequencies
+    iter_range: list floats
+      values of changing variable U or tp
+    berarange: real ndarray 1D, values of beta
+    entry: 0, 1 corresponds to diagonal or off-diagonal entry of function
+    label_head: string for label
+    ax, dx: matplotlib axis to plot in
+    """
+
     for j, u in enumerate(iter_range):
         dat = []
         for i, beta in list(enumerate(betarange)):
             tau, w_n = gf.tau_wn_setup(dict(BETA=beta, N_MATSUBARA=20))
             dat.append(np.polyfit(
-                w_n[:2], function_array[j][i][0].imag[:2], 1))
+                w_n[:2], function_array[j][i][entry][:2], 1))
         ax.plot(1 / BETARANGE, -np.array(dat)[:, 1], label=label_head + str(u))
         dx.plot(1 / BETARANGE, -np.array(dat)[:, 0], label=label_head + str(u))
 
 f, (si, ds) = plt.subplots(2, 2, sharex=True)
-plot_low_w(sigmasI_U, U_int, BETARANGE, 'INS U=', si[0], ds[0])
-plot_low_w(sigmasM_U, U_int, BETARANGE, 'MET U=', si[0], ds[0])
+plot_zero_w(sigmasI_U, U_int, BETARANGE, 0, 'INS U=', si[0], ds[0])
+plot_zero_w(sigmasM_U, U_int, BETARANGE, 0, 'MET U=', si[0], ds[0])
 
-ds[0].legend()
+plot_zero_w(sigmasI_U, U_int, BETARANGE, 1, 'INS U=', si[1], ds[1])
+plot_zero_w(sigmasM_U, U_int, BETARANGE, 1, 'MET U=', si[1], ds[1])
+
+si[0].legend(loc=0, prop={'size': 8})
+
 si[0].set_ylabel(r'-$\Im \Sigma_{AA}(w=0)$')
 ds[0].set_ylabel(r'-$\Im d\Sigma_{AA}(w=0)$')
-ds[0].set_xlim([0, .2])
+si[1].set_ylabel(r'-$\Im \Sigma_{AB}(w=0)$')
+ds[1].set_ylabel(r'-$\Im d\Sigma_{AB}(w=0)$')
+ds[0].set_xlim([0, .06])
+
+ds[0].set_xlabel('$T/D$')
+ds[1].set_xlabel('$T/D$')
 
 ###############################################################################
 # Changing tp in the METAL dome for metal and insulator Fix U
@@ -87,9 +112,19 @@ sigmasM_tp = Parallel(n_jobs=-1)(delayed(loop_beta)(2.7, tp, BETARANGE, 'M')
 sigmasI_tp = Parallel(n_jobs=-1)(delayed(loop_beta)(2.7, tp, BETARANGE, 'I')
                                  for tp in tp_r)
 
-plot_low_w(sigmasI_tp, tp_r, BETARANGE, 'INS tp=', si[1], ds[1])
-plot_low_w(sigmasM_tp, tp_r, BETARANGE, 'MET tp=', si[1], ds[1])
+f, (si, ds) = plt.subplots(2, 2, sharex=True)
+plot_zero_w(sigmasI_tp, tp_r, BETARANGE, 0, 'INS tp=', si[0], ds[0])
+plot_zero_w(sigmasM_tp, tp_r, BETARANGE, 0, 'MET tp=', si[0], ds[0])
 
-ds[1].legend()
-si[1].set_xlabel('$T/D$')
+plot_zero_w(sigmasI_tp, tp_r, BETARANGE, 1, 'INS tp=', si[1], ds[1])
+plot_zero_w(sigmasM_tp, tp_r, BETARANGE, 1, 'MET tp=', si[1], ds[1])
+
+si[0].set_ylabel(r'-$\Im \Sigma_{AA}(w=0)$')
+ds[0].set_ylabel(r'-$\Im d\Sigma_{AA}(w=0)$')
+si[1].set_ylabel(r'-$\Im \Sigma_{AB}(w=0)$')
+ds[1].set_ylabel(r'-$\Im d\Sigma_{AB}(w=0)$')
+ds[0].set_xlim([0, .06])
+
+si[0].legend(loc=0, prop={'size': 8})
+ds[0].set_xlabel('$T/D$')
 ds[1].set_xlabel('$T/D$')
