801256d345fd7bd718f134f652d3ad50f6df473d
Óscar Nájera <najera.oscar@gmail.com>
2016-04-01 15:31:57 +0200

    IPT single band now plots Self-Energy
    In real frequencies

--- examples/IPT/plot_ipt.py
+++ examples/IPT/plot_ipt.py
@@ -23,35 +23,57 @@ import matplotlib.pylab as plt
 
 U = 3.
 beta = 64.
-tau, w_n = tau_wn_setup(dict(BETA=beta, N_MATSUBARA=beta))
-omega = np.linspace(-3, 3, 200)
+tau, w_n = tau_wn_setup(dict(BETA=beta, N_MATSUBARA=3 * beta))
+omega = np.linspace(-3, 3, 600)
 g_iwn0 = greenF(w_n)
 
 fig_giw, giw_ax = plt.subplots()
 fig_gw, gw_ax = plt.subplots()
+fig_sw, sw_ax = plt.subplots(1, 2, sharex=True)
 
-g_iwn, _ = dmft_loop(U, 0.5, -1.j/w_n, w_n, tau)
+g_iwn, s_iwn = dmft_loop(U, 0.5, -1.j / w_n, w_n, tau, conv=1e-8)
 giw_ax.plot(w_n, g_iwn.imag, 's-', label='Insulator Solution')
 pade_coefs = pade_coefficients(g_iwn, w_n)
-gw_ax.plot(omega, -pade_rec(pade_coefs, omega, w_n).imag/np.pi, label='Insulator Solution')
+gw_ax.plot(omega, -pade_rec(pade_coefs, omega, w_n).imag /
+           np.pi, label='Insulator Solution')
 
+pade_coefs = pade_coefficients(s_iwn, w_n)
+sigma_w = pade_rec(pade_coefs, omega, w_n)
+sw_ax[0].plot(omega, sigma_w.real, label='Insulator Solution')
+sw_ax[1].plot(omega, sigma_w.imag, label='Insulator Solution')
 
-g_iwn, _ = dmft_loop(U, 0.5, g_iwn0, w_n, tau)
+g_iwn, s_iwn = dmft_loop(U, 0.5, g_iwn0, w_n, tau, conv=1e-8)
 giw_ax.plot(w_n, g_iwn.imag, 's-', label='Metal Solution')
 pade_coefs = pade_coefficients(g_iwn, w_n)
-gw_ax.plot(omega, -pade_rec(pade_coefs, omega, w_n).imag/np.pi, label='Metal Solution')
+gw_ax.plot(omega, -pade_rec(pade_coefs, omega, w_n).imag /
+           np.pi, label='Metal Solution')
 
-giw_ax.plot(w_n, -1/w_n, label='$1/w$ tail')
-giw_ax.plot(w_n, -1/w_n + U**2/4/w_n**3, label='$w^{-1}+ w^{-3}$ tail')
+pade_coefs = pade_coefficients(s_iwn, w_n)
+sigma_w = pade_rec(pade_coefs, omega, w_n)
+sw_ax[0].plot(omega, sigma_w.real, label='Metal Solution')
+sw_ax[1].plot(omega, sigma_w.imag, label='Metal Solution')
 
-giw_ax.set_xlim([0, max(w_n)])
-cut = int(6.5*beta/np.pi)
-giw_ax.set_ylim([g_iwn.imag[:cut].min()*1.1, 0])
-plt.legend(loc=0)
+giw_ax.plot(w_n, -1 / w_n, label='$1/w$ tail')
+giw_ax.plot(w_n, -1 / w_n + U**2 / 4 / w_n**3, label='$w^{-1}+ w^{-3}$ tail')
+
+cut = int(6.5 * beta / np.pi)
+giw_ax.set_xlim([0, 6.5])
+giw_ax.set_ylim([g_iwn.imag[:cut].min() * 1.1, 0])
+
 giw_ax.set_ylabel(r'$G(i\omega_n)$')
 giw_ax.set_xlabel(r'$i\omega_n$')
 giw_ax.set_title(r'$G(i\omega_n)$ at $\beta= {}$, $U= {}$'.format(beta, U))
+giw_ax.legend(loc=0)
 
 gw_ax.set_ylabel(r'$A(\omega)$')
 gw_ax.set_xlabel(r'$\omega$')
 gw_ax.set_title(r'Spectral function at $\beta= {}$, $U= {}$'.format(beta, U))
+gw_ax.legend(loc=0)
+
+sw_ax[0].set_ylabel(r'$\Re e \Sigma(\omega)$')
+sw_ax[1].set_ylabel(r'$\Im m \Sigma(\omega)$')
+sw_ax[0].set_xlabel(r'$\omega$')
+sw_ax[1].set_xlabel(r'$\omega$')
+sw_ax[0].set_title(r'Self-Energy at $\beta= {}$, $U= {}$'.format(beta, U))
+sw_ax[0].legend(loc=0)
+sw_ax[1].legend(loc=0)
