e303e9142555069eea11b44d220bed0ce4709eb9
Óscar Nájera <najera.oscar@gmail.com>
2016-04-05 17:25:53 +0200

    Hubbard III looking at Bonding Anti-bonding basis

--- examples/RKKY/plot_hubbard_III.py
+++ examples/RKKY/plot_hubbard_III.py
@@ -10,25 +10,71 @@ Describing the position of the Self-Energy pole in the diagonal basis
 # Author: Óscar Nájera
 from __future__ import absolute_import, division, print_function
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 import dmft.common as gf
+import dmft.RKKY_dimer as rt
+import slaveparticles.quantum.operators as op
+
 
 ###############################################################################
-# Spectral function approx
-# ------------------------
+# The Dimer limit
+# ---------------
+#
+# Here I study the shape of spectral function and the origin of its
+# poles looking for the zeros of the self energy
+
+def molecule_sigma_d(omega, U, mu, tp, beta):
+    """Return molecule self-energy in the given frequency axis"""
+
+    h_at, oper = rt.dimer_hamiltonian_bond(U, mu, tp)
+    oper_pair = [[oper[0], oper[0]], [oper[1], oper[1]]]
+
+    eig_e, eig_v = op.diagonalize(h_at.todense())
+    gfsU = np.array([op.gf_lehmann(eig_e, eig_v, c.T, beta, omega, d)
+                     for c, d in oper_pair])
+
+    plt.plot(omega.real, -(gfsU[1]).imag, label='Anti-Bond')
+    plt.plot(omega.real, -(gfsU[0]).imag, label='Bond')
+    plt.xlabel(r'$\omega$')
+    plt.ylabel(r'$A(\omega)$')
+    plt.title(r'Isolated dimer $U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, beta))
+    plt.legend(loc=0)
+
+    return [omega + tp - 1 / gfsU[0], omega - tp - 1 / gfsU[1]]
 
 
-w = np.linspace(-3, 3, 800)
-U = 2.15
+w = np.linspace(-3, 3, 1800)
+U = 2.5
 tp = 0.3
-sp_2 = (U**2 + 16 * tp**2) / 4.
-g0_1_b = w + tp + 1e-8j
-g0_1_a = w - tp + 1e-8j
 
-plt.plot(w, -(1 / (g0_1_a - sp_2 / g0_1_a)).imag)
-plt.plot(w, -(1 / (g0_1_b - sp_2 / g0_1_b)).imag)
+
+plt.figure()
+sig_b, sig_a = molecule_sigma_d(w + 5e-2j, U, 0, tp, 500)
+plt.plot(w, sig_a.real - w + tp, label=r'$\Sigma_{A} - w + t_\perp$')
+plt.plot(w, sig_b.real - w - tp, label=r'$\Sigma_{B} - w - t_\perp$')
+plt.legend(loc=0)
+
+###############################################################################
+# Hubbard III approximation
+# -------------------------
+#
+# Taking advantage of the rotated basis which is diagonal I can
+# independently treat each system on its own and solve 2 decoupled
+# system equations.
+#
+# In this case the poles of the green function are equally weighted,
+# only its position is correct, in comparison to the isolated dimer
+# green function
+
+sp_2 = (U**2 + 16 * tp**2) / 4.
+g0_1_a = w - tp + 5e-2j + 2 * tp  # The excitation out of the singlet has
+g0_1_b = w + tp + 5e-2j - 2 * tp  # this extra contribution of 2tp
+
+plt.figure()
+plt.plot(w, -(1 / (g0_1_a - sp_2 / g0_1_a)).imag, label='Anti-bond')
+plt.plot(w, -(1 / (g0_1_b - sp_2 / g0_1_b)).imag, label='Bond')
 plt.xlabel(r'$\omega$')
 plt.ylabel(r'$A(\omega)$')
 plt.title(r'Isolated dimer approximation $U={}$, $t_\perp={}$'.format(U, tp))
@@ -43,31 +89,33 @@ for i in range(2000):
 # ---------------
 
 plt.figure()
-plt.plot(w, (sp_2 / g0_1_b).real, label=r"Real")
-plt.plot(w, (sp_2 / g0_1_b).imag, label=r"Imag")
+plt.plot(w, (sp_2 / g0_1_b).real, label=r"Re Bond")
+plt.plot(w, (sp_2 / g0_1_b).imag, label=r"Im Bond")
 
-plt.plot(w, (sp_2 / g0_1_a).real, label=r"Real")
-plt.plot(w, (sp_2 / g0_1_a).imag, label=r"Imag")
+plt.plot(w, (sp_2 / g0_1_a).real, label=r"Re Anti-Bond")
+plt.plot(w, (sp_2 / g0_1_a).imag, label=r"Im Anti-Bond")
 
 plt.ylabel(r'$\Sigma(\omega)$')
 plt.xlabel(r'$\omega$')
 plt.title(r'$\Sigma(\omega)$ at $U= {}$'.format(U))
 plt.legend(loc=0)
-plt.ylim([-1.5, 1])
+plt.ylim([-3.5, 2])
 
 ###############################################################################
 # The Green Function
 # ------------------
 
 plt.figure()
-plt.plot(w, (1 / (w + tp - sp_2 / g0_1_b)).real, label=r"Real")
-plt.plot(w, (1 / (w + tp - sp_2 / g0_1_b)).imag, label=r"Imag")
-plt.plot(w, (1 / (w - tp - sp_2 / g0_1_a)).real, label=r"Real")
-plt.plot(w, (1 / (w - tp - sp_2 / g0_1_a)).imag, label=r"Imag")
+plt.plot(w, (1 / (w + tp - sp_2 / g0_1_b)).real, label=r"Re Bond")
+plt.plot(w, (1 / (w + tp - sp_2 / g0_1_b)).imag, label=r"Im Bond")
+plt.plot(w, (sp_2 / g0_1_b).real - w - tp, label=r'$\Sigma_{S} - w + t_\perp$')
+#plt.plot(w, (1 / (w - tp - sp_2 / g0_1_a)).real, label=r"Re Anti-Bond")
+#plt.plot(w, (1 / (w - tp - sp_2 / g0_1_a)).imag, label=r"Im Anti-Bond")
 
 plt.ylabel(r'$G(\omega)$')
 plt.xlabel(r'$\omega$')
 plt.title(r'$G(\omega)$ at $U= {}$'.format(U))
+plt.ylim([-3.5, 2])
 plt.legend(loc=0)
 
 ###############################################################################
