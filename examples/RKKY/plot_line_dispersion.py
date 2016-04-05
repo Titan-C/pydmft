474438518b909382f214cd6fdc17302502248098
Óscar Nájera <najera.oscar@gmail.com>
2016-04-05 10:37:09 +0200

    IPT dimer dispersions in Bonding bands

--- examples/RKKY/plot_line_dispersion.py
+++ examples/RKKY/plot_line_dispersion.py
@@ -45,11 +45,11 @@ def loop_u_tp(u_range, tprange, beta, seed='mott gap'):
 
 
 def pade_diag(gf_d, gf_o, w_n, w_set, w):
-    gf_s = 1j * gf_d.imag + gf_o
-    gf_a = 1j * gf_d.imag - gf_o
+    gf_s = 1j * gf_d.imag + gf_o  # Anti-bond
     pc = gf.pade_coefficients(gf_s[w_set], w_n[w_set])
     gr_s = gf.pade_rec(pc, w + 5e-8j, w_n[w_set])
 
+    gf_a = 1j * gf_d.imag - gf_o  # bond
     pc = gf.pade_coefficients(gf_a[w_set], w_n[w_set])
     gr_a = gf.pade_rec(pc, w + 5e-8j, w_n[w_set])
 
@@ -75,7 +75,74 @@ def plot_band_dispersion(w, Aw, title):
     plt.xlabel(r'$\epsilon$')
     plt.ylabel(r'$\omega$')
 
-urange = np.linspace(2.5, 5.5, 10)
+
+def plot_spectral(w, ss_w, sa_w, U, mu, tp, beta, plot_second):
+    f, ax = plt.subplots(1, sharex=True)
+    ax.plot(w, ss_w.real, ":", label='Re G sum')
+    ax.plot(w, -ss_w.imag, label='-Im G sum')
+    if plot_second:
+        ax[1].plot(w, sa_w.real, ":", label='Re G dif')
+        ax[1].plot(w, -sa_w.imag, label='Im G dif')
+    ax.legend(loc=0)
+    ax.set_xlabel(r'$\omega$')
+    ax.set_ylabel(r'$A_{Bond}(\omega)$')
+    ax.set_title(
+        r'Isolated dimer $U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, beta))
+    ax.set_ylim([-.5, 2])
+    plt.show()
+
+
+def plot_self_energy(w, ss_w, sa_w, U, mu, tp, beta, plot_second):
+    f, ax = plt.subplots(1, sharex=True)
+    ax.plot(w, ss_w.real, label='Re sum')
+    ax.plot(w, ss_w.imag, label='Im sum')
+    if plot_second:
+        ax[1].plot(w, sa_w.real, label='Re dif')
+        ax[1].plot(w, sa_w.imag, label='Im dif')
+    ax.legend(loc=0)
+    ax.set_xlabel(r'$\omega$')
+    ax.set_ylabel(r'$\Sigma_{Bond}(\omega)$')
+    ax.set_title(
+        r'Isolated dimer $U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, beta))
+    ax.set_ylim([-5, 2])
+    plt.show()
+
+
+def plot_dispersions(giw_s, sigma_iw, ur, tp, w_n, w):
+
+    for U, (giw_d, giw_o), (sig_d, sig_o) in zip(ur, giw_s, sigma_iw):
+        gs, ga = pade_diag(giw_d, giw_o, w_n, np.arange(150), w)
+        plot_spectral(w, gs, ga,
+                      U, 0, tp, 100., True)
+        ss, sa = pade_diag(sig_d, sig_o, w_n, np.arange(150), w)
+        plot_self_energy(w, ss, sa, U, 0, tp, 100., True)
+
+        lat_gfs = 1 / np.add.outer(-eps_k, w - tp + 5e-2j - ss)
+        lat_gfa = 1 / np.add.outer(-eps_k, w + tp + 5e-2j - sa)
+        Aw = np.clip(-.5 * (lat_gfa + lat_gfs).imag / np.pi, 0, 2)
+
+        plot_band_dispersion(
+            w, Aw, r'IPT lattice dimer $U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, beta))
+
+###############################################################################
+# Metals
+# ------
+
+urange = [1.5, 2., 2.175, 2.5, 3., 3.5]
+beta = 100.
+tp = 0.3
+giw_s, sigma_iw, w_n = loop_u_tp(
+    urange, tp * np.ones_like(urange), beta, "M")
+
+w = np.linspace(-4, 4, 800)
+eps_k = np.linspace(-1., 1., 61)
+plot_dispersions(giw_s, sigma_iw, urange, tp, w_n, w)
+
+###############################################################################
+# Insulators
+# ----------
+
+urange = [2.175, 2.5, 3., 3.5, 4., 5.]
 beta = 100.
 tp = 0.3
 giw_s, sigma_iw, w_n = loop_u_tp(
@@ -83,34 +150,4 @@ giw_s, sigma_iw, w_n = loop_u_tp(
 
 w = np.linspace(-4, 4, 800)
 eps_k = np.linspace(-1., 1., 61)
-
-
-def plot_self_energy(w, sd_w, so_w, U, mu, tp, beta):
-    f, ax = plt.subplots(2, sharex=True)
-    ax[0].plot(w, sd_w.real, label='Real')
-    ax[0].plot(w, sd_w.imag, label='Imag')
-    ax[1].plot(w, so_w.real, label='Real')
-    ax[1].plot(w, so_w.imag, label='Imag')
-    ax[0].legend(loc=0)
-    ax[1].set_xlabel(r'$\omega$')
-    ax[0].set_ylabel(r'$\Sigma_{AA}(\omega)$')
-    ax[1].set_ylabel(r'$\Sigma_{AB}(\omega)$')
-    ax[0].set_title(
-        r'Isolated dimer $U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, beta))
-    plt.show()
-
-
-def plot_dispersions(sigma_iw, ur, tp, w_n, w):
-
-    for U, (sig_d, sig_o) in zip(ur, sigma_iw):
-        sd, so = pade_diag(sig_d, sig_o, w_n, np.arange(120), w)
-        plot_self_energy(w, sd, so, U, 0, tp, 100.)
-
-        lat_gfs = 1 / np.add.outer(-eps_k, w - tp + 5e-2j - sd)
-        lat_gfa = 1 / np.add.outer(-eps_k, w + tp + 5e-2j - so)
-        Aw = np.clip(-.5 * (lat_gfa + lat_gfs).imag / np.pi, 0, 2)
-
-        plot_band_dispersion(
-            w, Aw, r'IPT lattice dimer $U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, beta))
-
-plot_dispersions(sigma_iw, urange, tp, w_n, w)
+plot_dispersions(giw_s, sigma_iw, urange, tp, w_n, w)
