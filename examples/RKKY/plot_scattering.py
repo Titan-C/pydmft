37ec1a19a2851909e70ca190019fc1fdd011a058
Óscar Nájera <najera.oscar@gmail.com>
2016-04-27 20:24:02 +0200

    Plot of dimer sigma scattering rate

--- examples/RKKY/plot_scattering.py
+++ examples/RKKY/plot_scattering.py
@@ -14,13 +14,16 @@ import itertools
 from math import log, ceil
 import numpy as np
 import matplotlib.pyplot as plt
+plt.matplotlib.rcParams.update({'axes.labelsize': 22,
+                                'xtick.labelsize': 14, 'ytick.labelsize': 14,
+                                'axes.titlesize': 22})
 from joblib import Parallel, delayed
 import dmft.RKKY_dimer as rt
 import dmft.common as gf
 import dmft.ipt_imag as ipt
 
 
-def loop_beta(u_int, tp, betarange, seed='mott gap'):
+def loop_beta(u_int, tp, betarange, seed='ins'):
     """Solves IPT dimer and return Im Sigma_AA, Re Simga_AB
 
     returns list len(betarange) x 2 Sigma arrays
@@ -29,14 +32,14 @@ def loop_beta(u_int, tp, betarange, seed='mott gap'):
     sigma_iw = []
     iterations = []
     for beta in betarange:
-        tau, w_n = gf.tau_wn_setup(
-            dict(BETA=beta, N_MATSUBARA=max(2**ceil(log(4 * beta) / log(2)), 256)))
+        tau, w_n = gf.tau_wn_setup(dict(BETA=beta, N_MATSUBARA=2**12))
 
         giw_d, giw_o = rt.gf_met(w_n, 0., 0., 0.5, 0.)
-        if seed == 'I':
+        if seed == 'ins':
             giw_d, giw_o = 1 / (1j * w_n - 4j / w_n), np.zeros_like(w_n) + 0j
+
         giw_d, giw_o, loops = rt.ipt_dmft_loop(
-            beta, u_int, tp, giw_d, giw_o, tau, w_n, 1e-8)
+            beta, u_int, tp, giw_d, giw_o, tau, w_n, 1e-5)
         iterations.append(loops)
         g0iw_d, g0iw_o = rt.self_consistency(
             1j * w_n, 1j * giw_d.imag, giw_o.real, 0., tp, 0.25)
@@ -47,20 +50,72 @@ def loop_beta(u_int, tp, betarange, seed='mott gap'):
 
     return sigma_iw
 
-temp = np.arange(1 / 500., .061, 1 / 250)
+temp = np.arange(1 / 500., .05, .001)
 BETARANGE = 1 / temp
 
 ###############################################################################
 # Calculate in the METAL dome the metal and insulator solution fix tp
 
-U_int = [2.5, 3., 3.5]
-sigmasM_U = Parallel(n_jobs=-1)(delayed(loop_beta)(u_int, .3, BETARANGE, 'M')
-                                for u_int in U_int)
-
-sigmasI_U = Parallel(n_jobs=-1)(delayed(loop_beta)(u_int, .3, BETARANGE, 'I')
-                                for u_int in U_int)
 
 # Plot a 2x2 frame following sigma and d_sigma Changing U
+
+
+def plot_zero_w(function_array, iter_range, tp, betarange, ax):
+    """Plot the zero frequency extrapolation of a function
+    Parameters
+    ----------
+    function_array: real ndarray
+      contains the function (G, Sigma) to linearly fit over 2 first frequencies
+    iter_range: list floats
+      values of changing variable U or tp
+    berarange: real ndarray 1D, values of beta
+    entry: 0, 1 corresponds to diagonal or off-diagonal entry of function
+    label_head: string for label
+    ax, dx: matplotlib axis to plot in
+    """
+
+    sig_11_0 = []
+    rtp = []
+    for j, u in enumerate(iter_range):
+        sig_11_0.append([])
+        rtp.append([])
+        for i, beta in list(enumerate(betarange)):
+            tau, w_n = gf.tau_wn_setup(dict(BETA=beta, N_MATSUBARA=2))
+            sig_11_0[j].append(np.polyfit(
+                w_n, function_array[j][i][0][:2], 1)[1])
+            rtp[j].append(np.polyfit(w_n, function_array[j][i][1][:2], 1)[1])
+
+        ax[0].plot(1 / BETARANGE, -np.array(sig_11_0[j]), 'k', label=str(u))
+        ax[1].plot(1 / BETARANGE, tp + np.array(rtp[j]), 'k', label=str(u))
+    ax[0].set_ylabel(r'$-\Im m \Sigma_{11}(w=0)$')
+    ax[1].set_ylabel(r'$t_\perp + \Re e\Sigma_{12}(w=0)$')
+    ax[1].set_xlabel('$T/D$')
+    ax[1].set_xlim([min(temp), max(temp)])
+    return np.array(sig_11_0)
+
+U_intm = np.arange(.5, 3.1, .4)
+sigmasM_U = Parallel(n_jobs=-1)(delayed(loop_beta)
+                                (u_int, .3, BETARANGE, 'met') for u_int in U_intm)
+
+fig, si = plt.subplots(2, 1, sharex=True)
+sig_11_0 = plot_zero_w(sigmasM_U, U_intm, .3, BETARANGE, si)
+
+ax = fig.add_axes([.18, .6, .2, .3])
+ax.plot(U_intm, -sig_11_0[:, -1])
+ax.set_xticks(np.arange(.5, 3, 1))
+
+
+U_inti = [2.5, 3., 3.7]
+f, si = plt.subplots(2, 1, sharex=True)
+sigmasI_U = Parallel(n_jobs=-1)(delayed(loop_beta)(u_int, .3, BETARANGE, 'ins')
+                                for u_int in U_inti)
+plot_zero_w(sigmasI_U, U_inti, .3, BETARANGE, si)
+
+
+# si[0].legend(loc=0)
+
+
+###############################################################################
 
 
 def plot_zero_w(function_array, iter_range, betarange, entry, label_head, ax, dx):
