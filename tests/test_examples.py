e8097ca270f6bea443a64d23f0c06d30bc93b41a
Óscar Nájera <najera.oscar@gmail.com>
2016-04-29 16:04:48 +0200

    Blacklist examples to test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -23,19 +23,23 @@ import dmft.plot.hf_dimer as pdp
                           ('dimer_pm', 'testdp', pdp)])
 def test_example(case, ofile, plot):
     """Testing a very fast HF runs"""
-    command = "examples/Hirsh-Fye/{}.py -sweeps 100 -therm 400 -Niter 2 -ofile /tmp/{}".format(case, ofile)
+    command = "examples/Hirsh-Fye/{}.py -sweeps 100 -therm 400 -Niter 2 -ofile /tmp/{}".format(
+        case, ofile)
     command = command.split()
     assert not subprocess.call(command)
-    plot.show_conv(4, 2.5, filestr='/tmp/'+ofile, xlim=8, skip=0)
+    plot.show_conv(4, 2.5, filestr='/tmp/' + ofile, xlim=8, skip=0)
 
 
-plot_list = [pfl for pfl in os.listdir('examples') if pfl.startswith('plot')
-                                                    and pfl.endswith('.py')]
-plot_list += [pfl for pfl in os.listdir('examples/IPT') if pfl.startswith('plot')
-                                                    and pfl.endswith('.py')]
+skip_list = ['energies', 'stability', 'scattering']
+example_files = os.listdir(
+    'examples') + os.listdir('examples/IPT') + os.listdir('examples/RKKY')
+plot_list = [pfl for pfl in example_files if pfl.startswith(
+    'plot') and pfl.endswith('.py') and not any(x in pfl for x in skip_list)]
 sys.path.append('examples')
 sys.path.append('examples/IPT')
 sys.path.append('examples/RKKY')
+
+
 @pytest.mark.parametrize("plot", plot_list)
 def test_plots(plot):
-    exec('import '+plot[:-3])
+    exec('import ' + plot[:-3])
