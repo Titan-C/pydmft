d5a1658bfac2aeb41c89b8fab153da5b20d22439
Óscar Nájera <najera.oscar@gmail.com>
2016-05-26 03:41:54 +0200

    Calculate optical conductivity in dimer
    Using the diagonal basis bands and a well defined current operator

--- examples/RKKY/plot_optical_conductivity_bonding.py
+++ examples/RKKY/plot_optical_conductivity_bonding.py
@@ -44,125 +44,64 @@ def loop_u_tp(u_range, tprange, beta, seed='mott gap'):
     return np.array(giw_s), np.array(sigma_iw), w_n
 
 
-def plot_optical_cond(giw_s, sigma_iw, ur, tp, w_n, w, w_set, beta):
+def plot_optical_cond(giw_s, sigma_iw, ur, tp, w_n, w, w_set, beta, seed):
     nuv = w[w > 0]
     zerofreq = len(nuv)
     dw = w[1] - w[0]
-    de = eps_k[1] - eps_k[0]
+    E = np.linspace(-1, 1, 300)
+    dos = np.exp(-2 * E**2) / np.sqrt(np.pi / 2)
+    de = E[1] - E[0]
+    dosde = (dos * de).reshape(-1, 1)
     nf = fermi_dist(w, beta) - fermi_dist(np.add.outer(nuv, w), beta)
+    eta = 0.8
 
     for U, (giw_d, giw_o), (sig_d, sig_o) in zip(ur, giw_s, sigma_iw):
         gs, ga = rt.pade_diag(giw_d, giw_o, w_n, w_set, w)
         ss, sa = rt.pade_diag(sig_d, sig_o, w_n, w_set, w)
 
-        lat_gfs = 1 / np.add.outer(-eps_k, w - tp + 4e-3j - ss)
-        lat_gfa = 1 / np.add.outer(-eps_k, w + tp + 4e-3j - sa)
+        lat_Aa = (-1 / np.add.outer(-E, w + tp + 5e-3j - sa)).imag / np.pi
+        lat_As = (-1 / np.add.outer(-E, w - tp + 5e-3j - ss)).imag / np.pi
+        #lat_Aa = .5 * (lat_Aa + lat_As)
+        #lat_As = lat_Aa
 
-        Awa = -lat_gfa.imag / np.pi
-        Aws = -lat_gfs.imag / np.pi
+        a = np.array([[np.sum(lat_Aa[e] * np.roll(lat_Aa[e], -i) * nf[i])
+                       for i in range(len(nuv))] for e in range(len(E))])
+        a += np.array([[np.sum(lat_As[e] * np.roll(lat_As[e], -i) * nf[i])
+                        for i in range(len(nuv))] for e in range(len(E))])
+        b = np.array([[np.sum(lat_Aa[e] * np.roll(lat_As[e], -i) * nf[i])
+                       for i in range(len(nuv))] for e in range(len(E))])
+        b += np.array([[np.sum(lat_As[e] * np.roll(lat_Aa[e], -i) * nf[i])
+                        for i in range(len(nuv))] for e in range(len(E))])
+        b *= tp**2 * eta**2 / 2 / .25
 
-        ss = np.array([[np.sum(Aws[e] * np.roll(Aws[e], -i) * nf[i])
-                        for i in range(len(nuv))] for e in range(len(eps_k))]) / nuv
-        reconds = (bethe_lattice(eps_k, .5).reshape(-1, 1)
-                   * ss).sum(axis=0) * de * dw
+        sigma_E_sum = (dosde * (a)).sum(axis=0) * dw / nuv
+        plt.plot(nuv, sigma_E_sum, '--')
+        sigma_E_sum = (dosde * (b)).sum(axis=0) * dw / nuv
+        plt.plot(nuv, sigma_E_sum, ':')
+        sigma_E_sum = (dosde * (a + b)).sum(axis=0) * dw / nuv
+        plt.plot(nuv, sigma_E_sum)
 
-        sa = np.array([[np.sum(Awa[e] * np.roll(Awa[e], -i) * nf[i])
-                        for i in range(len(nuv))] for e in range(len(eps_k))]) / nuv
-        reconda = (bethe_lattice(eps_k, .5).reshape(-1, 1)
-                   * sa).sum(axis=0) * de * dw
+        # To save data manually at some point
+        np.savez('opt_cond{}'.format(seed), nuv=nuv, sigma_E_sum=sigma_E_sum)
 
-        title = r'IPT lattice dimer Bonding Conduct $U={}$, $t_\perp={}$, $\beta={}$'.format(
-            U, tp, BETA)
-
-        plt.figure()
-        plt.plot(nuv, reconds, label='sym')
-        plt.plot(nuv, reconda, label='asym')
-        plt.plot(nuv, reconds + reconda, label='sum')
-        plt.legend(loc=0)
         plt.xlabel(r'$\nu$')
         plt.ylabel(r'$\sigma(\nu)$')
-        plt.title(title)
-        plt.ylim([0, .1])
+        # plt.title(title)
+        plt.ylim([0, .6])
+        plt.xlim([0, 3])
 
 
 ###############################################################################
 # Metals
 # ------
 #
-urange = [2]  # [1.5, 2., 2.175, 2.5, 3.]
+urange = [2.5]  # [1.5, 2., 2.175, 2.5, 3.]
 BETA = 100.
 tp = 0.3
-giw_s, sigma_iw, w_n = loop_u_tp(urange, tp * np.ones_like(urange), BETA, "M")
-w = np.linspace(-8, 8, 800)
-eps_k = np.linspace(-1., 1., 61)
+w = np.linspace(-6, 6, 800)
 w_set = np.concatenate((np.arange(100), np.arange(100, 200, 2)))
-plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set, BETA)
+giw_s, sigma_iw, w_n = loop_u_tp(urange, tp * np.ones_like(urange), BETA, "M")
+plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set, BETA, 'met')
 
-###############################################################################
-# Insulators
-# ----------
-tp = 0.3
-urange = [2.175, 2.5, 3., 3.5, 4., 5.][::-1]
-giw_s, sigma_iw, w_n = loop_u_tp(
-    urange, tp * np.ones_like(urange), BETA)
-
-eps_k = np.linspace(-1., 1., 61)
-plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set, BETA)
-
-###############################################################################
-# Insulators
-# ----------
-tp = 0.5
-urange = [2.175, 2.5, 3., 3.5, 4., 5.][::-1]
-giw_s, sigma_iw, w_n = loop_u_tp(
-    urange, tp * np.ones_like(urange), BETA)
-
-eps_k = np.linspace(-1., 1., 61)
-plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set, BETA)
-###############################################################################
-# Insulators
-# ----------
-tp = 0.8
-urange = [1.5, 2., 2.5, 3., 3.5, 4., 5.][::-1]
-giw_s, sigma_iw, w_n = loop_u_tp(
-    urange, tp * np.ones_like(urange), BETA)
-
-eps_k = np.linspace(-1., 1., 61)
-plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set, BETA)
-
-###############################################################################
-# Insulators
-# ----------
-tp = 0.95
-urange = [1.2, 1.5, 2., 2.5, 3., 3.5, 4., 5.][::-1]
-giw_s, sigma_iw, w_n = loop_u_tp(
-    urange, tp * np.ones_like(urange), BETA)
-
-eps_k = np.linspace(-1., 1., 61)
-plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set, BETA)
-
-
-#urange = [3.2]
-#beta = 100.
-#tp = 0.2
-# giw_s, sigma_iw, w_n = loop_u_tp(
-# urange, tp * np.ones_like(urange), beta)
-#
-#w = np.linspace(-8, 8, 1600)
-#w_set = np.concatenate((np.arange(80), np.arange(90, 140, 25)))
-#eps_k = np.linspace(-2., 1., 61)
-#ss, gs = plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set)
-#
-#
-#tau, w_n = gf.tau_wn_setup(dict(BETA=beta, N_MATSUBARA=max(5 * beta, 256)))
-#tp = 0.2
-# g0iw_d, g0iw_o = rt.self_consistency(
-# 1j * w_n, giw_s[0], giw_s[0], 0., tp, 0.25)
-#siw_d, siw_o = ipt.dimer_sigma(urange[0], tp, g0iw_d, g0iw_o, tau, w_n)
-#giw_d, giw_o = rt.dimer_dyson(g0iw_d, g0iw_o, siw_d, siw_o)
-# g0iw_d, g0iw_o = rt.self_consistency(
-# 1j * w_n, giw_s[0], giw_s[0], 0., tp, 0.25)
-#siw_d, siw_o = ipt.dimer_sigma(urange[0], tp, g0iw_d, g0iw_o, tau, w_n)
-# ss, gs = plot_optical_cond(
-# np.array([giw_d, giw_o]), np.array([siw_d, siw_o]), urange, tp, w_n, w,
-# w_set)
+giw_s, sigma_iw, w_n = loop_u_tp(urange, tp * np.ones_like(urange), BETA)
+plot_optical_cond(giw_s, sigma_iw, urange, tp, w_n, w, w_set, BETA, 'ins')
