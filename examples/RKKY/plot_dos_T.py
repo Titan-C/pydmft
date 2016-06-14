84f2e103e162d69ac5ca31f3f9043da35222b03b
Óscar Nájera <najera.oscar@gmail.com>
2016-06-14 15:45:31 +0200

    Start IPT dimer to study DOS evolution with T

---
+++ examples/RKKY/plot_dos_T.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+r"""
+===========================================
+Evolution of DOS as function of temperature
+===========================================
+
+"""
+# Created Tue Jun 14 15:44:38 2016
+# Author: Óscar Nájera
+
+from __future__ import division, absolute_import, print_function
