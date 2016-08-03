252ac4f4cb46604ade7cbab4e83bf9aea7ad1bea
Óscar Nájera <najera.oscar@gmail.com>
2016-08-04 00:38:33 +0200

    Plot on dimer Mott insulator diagonal basis

--- examples/RKKY/plot_mott_insulator.py
+++ examples/RKKY/plot_mott_insulator.py
@@ -46,13 +46,13 @@ BETA = 100.
 
 tau, w_n = gf.tau_wn_setup(dict(BETA=BETA, N_MATSUBARA=1024))
 giw_d, giw_o = 1 / (1j * w_n + 4j / w_n), np.zeros_like(w_n) + 0j
-w = np.linspace(-4, 4, 2**8)
-w_set = np.concatenate((np.arange(100), np.arange(100, 200, 2)))
+w = np.linspace(-4, 4, 2**13 + 1)
 
 
 giw_dt0, giw_ot0, siw_dt0, siw_ot0 = dmft_solve(
     giw_d, giw_o, BETA, u_int, 0, tau, w_n)
 
+w_set = np.concatenate((np.arange(80), np.arange(80, 150, 5)))
 gwdt0 = gf.pade_continuation(1j * giw_dt0.imag, w_n, w, w_set)
 swdt0 = gf.pade_continuation(1j * siw_dt0.imag, w_n, w, w_set)
 swdt0 = swdt0.real - 1j * np.abs(swdt0.imag)
@@ -74,32 +74,71 @@ axes[2, 0].set_ylabel(r'$-\Im m \Sigma_{11}(\omega)$')
 
 for ax in axes.flatten():
     ax.set_yticks([])
-for ax, lim in zip(axes, [[0, 2], [-4, 4], [0, 2]]):
+for ax, lim in zip(axes, [[0, 2], [-8, 8], [0, 2]]):
     ax[0].set_ylim(lim)
     ax[1].set_ylim(lim)
 
 axes[2, 0].set_xlabel(r'$\omega$')
 axes[2, 1].set_xlabel(r'$\omega$')
+plt.savefig('IPT_imag_Dimer_mott_ins_panels.pdf', transparent=False,
+            bbox_inches='tight', pad_inches=0.05)
+###############################################################################
+# Insulator in IPT Imag diagonal basis
+# ------------------------------------
+#
+w_set = np.concatenate((np.arange(80), np.arange(80, 150, 5)))
+gwst0 = gf.pade_continuation(1j * giw_dt0.imag + giw_ot0.real, w_n, w, w_set)
+swst0 = gf.pade_continuation(1j * siw_dt0.imag + siw_ot0.real, w_n, w, w_set)
+#swst0 = swst0.real - 1j * np.abs(swst0.imag)
 
+
+gwst03 = gf.pade_continuation(
+    1j * giw_dt03.imag + giw_ot03.real, w_n, w, w_set)
+swst03 = gf.pade_continuation(
+    1j * siw_dt03.imag + siw_ot03.real, w_n, w, w_set)
+#swst03 = swst03.real - 1j * np.abs(swst03.imag)
+
+fig, axes = plt.subplots(3, 2, sharex=True)
+fig.subplots_adjust(hspace=0, wspace=0.0)
+plot_gf(gwst0, swst0, axes[:, 0])
+#plot_gf(gwst03, swst03, axes[:, 1])
+plot_gf(gf.semi_circle_hiltrans(w - 0.3 - swst03), swst03, axes[:, 1])
+axes[0, 0].set_ylabel(r'$A_{sym}(\omega)$')
+axes[1, 0].set_ylabel(r'$\Re e \Sigma_{sym}(\omega)$')
+axes[2, 0].set_ylabel(r'$-\Im m \Sigma_{sym}(\omega)$')
+
+for ax in axes.flatten():
+    ax.set_yticks([])
+for ax, lim in zip(axes, [[0, 2], [-8, 8], [0, 2]]):
+    ax[0].set_ylim(lim)
+    ax[1].set_ylim(lim)
+axes[2, 1].set_ylim([0, 2.6])
+
+axes[2, 0].set_xlabel(r'$\omega$')
+axes[2, 1].set_xlabel(r'$\omega$')
+plt.savefig('IPT_imag_Dimer_mott_ins_panels_sym.pdf', transparent=False,
+            bbox_inches='tight', pad_inches=0.05)
 ###############################################################################
 # Insulator in real IPT diagonal basis
 # ------------------------------------
 #
 w = np.linspace(-6, 6, 2**13)
 dw = w[1] - w[0]
+print(dw)
+BETA = 100.
 nfp = dos.fermi_dist(w, BETA)
 gseeds = gf.semi_circle_hiltrans(w + 1e-6j + 1.2)
 gseeda = gf.semi_circle_hiltrans(w + 1e-6j - 1.2)
-(gsst0, _), (sst0, _) = dimer_dmft_real(u_int, 0.3, nfp, w, dw, gseeds, gseeda)
+(gsst0, _), (sst0, _) = dimer_dmft_real(u_int, 0., nfp, w, dw, gseeds, gseeda)
 
-(gsst03, _), (sst03, _) = dimer_dmft_real(
+(gsst03, ga), (sst03, sa) = dimer_dmft_real(
     u_int, 0.3, nfp, w, dw, gseeds, gseeda)
 
 
 fig, axes = plt.subplots(3, 2, sharex=True)
 fig.subplots_adjust(hspace=0, wspace=0.0)
 plot_gf(gsst0, sst0, axes[:, 0])
-plot_gf(gsst03, sst03, axes[:, 1])
+plot_gf(gsst03 + ga, sst03 + sa, axes[:, 1])
 axes[0, 0].set_ylabel(r'$A_{sym}(\omega)$')
 axes[1, 0].set_ylabel(r'$\Re e \Sigma_{sym}(\omega)$')
 axes[2, 0].set_ylabel(r'$-\Im m \Sigma_{sym}(\omega)$')
@@ -113,3 +152,7 @@ for ax, lim in zip(axes, [[0, 2], [-4, 4], [0, 4]]):
 axes[2, 0].set_xlabel(r'$\omega$')
 axes[2, 1].set_xlabel(r'$\omega$')
 plt.xlim([-4, 4])
+
+plt.savefig('IPT_real_Dimer_mott_ins_panels_sym.pdf', transparent=False,
+            bbox_inches='tight', pad_inches=0.05)
+plt.show()
