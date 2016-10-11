633512f62d9310dbcd7317f382c7917476f7e008
Óscar Nájera <najera.oscar@gmail.com>
2016-10-11 23:49:34 +0200

    test potential energy dimer

--- tests/test_rkky.py
+++ tests/test_rkky.py
@@ -40,16 +40,25 @@ def test_hamiltonian_eigen_energies(u_int, mu, tp):
 
 
 @pytest.mark.parametrize("u_int, mu, tp",
-                         [(1, 0, 0.3), (2, 0, 0.5), (3, 0., 0.7)])
-def test_ekin(u_int, mu, tp, beta=100):
+                         [(1, 0, 0.3), (2, 0, 0.5), (3, 0., 0.8)])
+def test_dimer_energies(u_int, mu, tp, beta=100):
     h_loc, (a_up, b_up, a_dw, b_dw) = rt.dimer_hamiltonian(u_int, mu, tp)
     e_imp = (tp * (a_up.T * b_up + a_dw.T * b_dw +
                    b_up.T * a_up + b_dw.T * a_dw)).todense()
     eig_e, eig_v = op.diagonalize(h_loc.todense())
-    w_n = gf.matsubara_freq(beta, 2**7)  # n=2**7=128
+    w_n = gf.matsubara_freq(beta, 2**8)  # n=2**7=256
     gf_di = op.gf_lehmann(eig_e, eig_v, a_up.T, beta, 1j * w_n)
     gf_of = op.gf_lehmann(eig_e, eig_v, a_up.T, beta, 1j * w_n, b_up)
     ekin_gf = rt.ekin(gf_di, gf_of, w_n, tp, beta, 0)
     ekin_ed = op.expected_value(e_imp, eig_e, eig_v, beta)
 
-    assert abs(ekin_ed - ekin_gf) < 1e-3
+    assert abs(ekin_ed - ekin_gf) < 5e-4
+
+    epot_gf = rt.epot(gf_di, w_n, beta, u_int**2 / 4 + tp**2, ekin_gf, u_int)
+
+    docc = (a_up.T * a_up * a_dw.T * a_dw +
+            b_up.T * b_up * b_dw.T * b_dw).todense()
+
+    epot_ed = op.expected_value(docc, eig_e, eig_v, beta) * u_int
+
+    assert abs(epot_ed - epot_gf) < 1e-3
