91560eb740bb9ea36ac7baefab2f0f151d9523e6
Óscar Nájera <najera.oscar@gmail.com>
2017-02-03 18:59:13 +0100

    FIX util script to generate seeds for high temperatures

--- examples/dimer_bethe/save_triqsgf.py
+++ examples/dimer_bethe/save_triqsgf.py
@@ -12,7 +12,7 @@ from __future__ import division, absolute_import, print_function
 import numpy as np
 import matplotlib.pyplot as plt
 
-from pytriqs.gf.local import *
+from pytriqs.gf.local import GfImFreq, BlockGf
 from pytriqs.archive import HDFArchive
 
 import dmft.common as gf
@@ -20,16 +20,16 @@ import py3qs.dimer_lattice as dlat
 from pymaxent.tools import hilbert_trans, differential_weight
 
 tp = 0.3
-u_str = 'U1.8'
-#u_str = 'U2.01'
-#u_str = 'U2.15'
+u_int = 1.8
+# u_int = 2.01
+# u_int = 2.15
 
-data = np.load('AwmetB25tp0.3{}G_sym.npz'.format(u_str))
+data = np.load('AwmetB25tp0.3U{}G_sym.npz'.format(u_int))
 Aw = data['Aw']
 w = data['w']
 
-# plt.plot(w, Aw)
-# plt.show()
+plt.plot(w, Aw)
+plt.show()
 
 ###############################################################################
 # High temperature seeds
@@ -45,19 +45,18 @@ for beta in hot_beta:
     print(beta)
     gfsiw.append(hilbert_trans(1j * wnh, w, differential_weight(w), Aw, 0))
     plt.plot(wnh, gfsiw[-1].imag, 'o:')
+    plt.plot(wnh, gfsiw[-1].real, 'o:')
 
     # triqs blocks
     gfarr = GfImFreq(indices=[0], beta=beta, n_points=int(beta * 3))
     G_iw = BlockGf(name_list=['asym_dw', 'asym_up', 'sym_dw', 'sym_up'],
                    block_list=(gfarr, gfarr, gfarr, gfarr), make_copies=True)
-
-    for name, gblock in G_iw:
-        if 'sym' in name:
-            gblock.data[:, 0, 0] = gfsiw[-1]
-        else:
-            gblock.data[:, 0, 0] = -gfsiw[-1].conj()
-
-    dlat.paramagnetic_hf_clean(G_iw, float(u_str[1:]), tp)
+    dlat.gf_sym_2_triqs_blockgf(gfsiw[-1], G_iw, u_int, tp)
 
     with HDFArchive('DIMER_PM_met_B{}_tp0.3.h5'.format(beta), 'a') as dest:
-        dest['/{}/it000/G_iw'.format(u_str)] = G_iw
+        dest['/U{}/it000/G_iw'.format(u_int)] = G_iw
+
+# block_names = ['sym_up', 'sym_dw', 'asym_up', 'asym_dw']
+# gref = np.squeeze([G_iw[name].data for name in block_names])
+# plt.plot(wnh, gref.T.real)
+# plt.plot(wnh, gref.T.imag)
