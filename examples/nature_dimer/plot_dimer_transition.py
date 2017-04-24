96f4dac4126a5bf91c5f8ee83240121c247c2c92
Óscar Nájera <najera.oscar@gmail.com>
2017-04-24 17:37:13 +0200

    tp 0.8 dimer transition

--- examples/nature_dimer/plot_dimer_transition.py
+++ examples/nature_dimer/plot_dimer_transition.py
@@ -32,6 +32,10 @@ dw = w[1] - w[0]
 
 beta = 800.
 nfp = gf.fermi_dist(w, beta)
+###############################################################################
+# The :math:`t_\perp/D=0.3` scenario
+# ==================================
+#
 
 tp = 0.3
 gss = gf.semi_circle_hiltrans(w + 5e-3j - tp)
@@ -55,3 +59,31 @@ plt.xlim([-4, 4])
 plt.ylim([shift, 2.1])
 plt.yticks(0.5 - 2.1 * np.arange(len(urange)), ['U=' + str(u) for u in urange])
 # plt.savefig('dimer_transition_spectra.pdf')
+
+###############################################################################
+# The :math:`t_\perp/D=0.8` scenario
+# ==================================
+#
+tp = 0.8
+gss = gf.semi_circle_hiltrans(w + 5e-3j - tp)
+gsa = gf.semi_circle_hiltrans(w + 5e-3j + tp)
+urange = np.linspace(0.2, 1.64, 6)
+urange = [0.5, 1., 1.2, 1.352, 1.4, 2.5, 3]
+plt.close('all')
+for i, U in enumerate(urange):
+    (gss, gsa), (ss, sa) = ipt.dimer_dmft(
+        U, tp, nfp, w, dw, gss, gsa, conv=1e-4)
+
+    plt.gca().set_prop_cycle(None)
+    shift = -2.1 * i
+    plt.plot(w, shift + -gss.imag)
+    plt.plot(w, shift + -gsa.imag)
+    plt.plot(w, shift + -(gss + gsa).imag / 2, 'k', lw=2)
+    plt.axhline(shift, color='k', lw=0.5)
+
+plt.xlabel(r'$\omega$')
+plt.xlim([-4, 4])
+plt.ylim([shift, 2.1])
+plt.yticks(0.5 - 2.1 * np.arange(len(urange)), ['U=' + str(u) for u in urange])
+
+# plt.savefig('dimer_transition_spectra_tp0.8.pdf')
