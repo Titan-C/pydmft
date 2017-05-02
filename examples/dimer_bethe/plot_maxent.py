f28e9134d30c50eb4db30a8ce64944f4fb71adae
Óscar Nájera <najera.oscar@gmail.com>
2017-05-02 14:13:58 +0200

    Plot Matsubara self-energy in maxent

--- examples/dimer_bethe/plot_maxent.py
+++ examples/dimer_bethe/plot_maxent.py
@@ -11,26 +11,22 @@ Here I continue an insulator
 from __future__ import division, absolute_import, print_function
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.interpolate import interp1d
 import dmft.common as gf
 from dmft import dimer
 from pymaxent.maxent import Maxent
 from pymaxent.draw import gf_plot_study
 from pymaxent.tools import gaussian, hilbert_trans, differential_weight
 
-# plt.matplotlib.rcParams.update({'axes.labelsize': 22, 'xtick.labelsize':
-# 14, 'ytick.labelsize': 14, 'axes.titlesize': 22})
-
 # Output for G_sym= G_AA+G_AB
 # G0 = w -tp - (G_sym)
 # S = w -tp - t^2 G_sym - G_sym^-1
 
 fac = np.arctan(10 * np.sqrt(3) / 2.5)
-omega = np.tan(np.linspace(-fac, fac, 321)) * 2.5 / np.sqrt(3)
+omega = np.tan(np.linspace(-fac, fac, 351)) * 2.5 / np.sqrt(3)
 
-tp = 0.1
-BETA = 200.
-u_int = 2.29
+tp = 0.5
+BETA = 100.
+u_int = 1.67
 u_str = 'U' + str(u_int)
 workdir = "/home/oscar/orlando/dev/dmft-learn/examples/dimer_bethe/tp03f/"
 workdir = "/home/oscar/orlando/dev/dmft-learn/examples/dimer_bethe/"
@@ -46,13 +42,17 @@ giw = giw.reshape(-1, nfreq)
 gerr = giw.std(0).clip(3e-4)
 
 
-defaultM = gaussian(omega, tp, 0.25 + u_int**2 / 4)
+defaultM = gaussian(omega, tp, tp**2 + 0.25 + u_int**2 / 4)
 
 Model_gw = Maxent(omega=omega, defaultModel=defaultM, tol=1e-5,
                   minimizer='Bryan', w_n=wn, giw=giw.mean(0),
                   giw_std=gerr, max_nfreq=int(2 * BETA))
-Model_gw.getAllSpecFs(alphamin=0.6, alphamax=2.5, numAlpha=24)
-gf_plot_study(omega, Model_gw)
+Model_gw.getAllSpecFs(alphamin=0.6, alphamax=2, numAlpha=24)
+
+fig, ax = plt.subplots(1, 2, gridspec_kw=dict(
+    wspace=0.05, hspace=0.1, width_ratios=[3, 1]))
+gf_plot_study(omega, Model_gw, "{AB}", ax=ax)
+ax[0].set_title(r'$U={}$, $t_\perp={}$, $\beta={}$'.format(u_int, tp, BETA))
 plt.show()
 
 # np.savez('AwmetB25tp0.3{}G_sym'.format(u_str), Aw = Model_gw.aveSpecFs, w = Model_gw.omega)
@@ -60,17 +60,24 @@ plt.show()
 ###############################################################################
 # How was the Green function fitted
 # ---------------------------------
-plt.figure('gffit')
+gs, gs_ax = plt.subplots(2, 1, sharex=True)
 gfit = Model_gw.restoreG(Model_gw.aveSpecFs)
 wnfit = Model_gw.wn
 
-plt.plot(wn, giw.mean(0).real, 'o:')
-plt.plot(wn, giw.mean(0).imag, 'o:')
+gs_ax[0].plot(wn, giw.mean(0).real, 's:', label=r'$\Re e$')
+gs_ax[0].plot(wn, giw.mean(0).imag, 'o:', label=r'$\Im m$')
+gs_ax[0].plot(wnfit, gfit.real, 'xk', label=r'MEM')
+gs_ax[0].plot(wnfit, gfit.imag, 'xk')
+gs_ax[0].set_ylabel(r'$\bar{G}_{AB}$')
+gs_ax[0].legend()
 
-plt.plot(wnfit, gfit.real, 'x')
-plt.plot(wnfit, gfit.imag, 'x')
+siw = 1j * wnfit - tp - 0.25 * gfit - 1 / gfit
+gs_ax[1].plot(wnfit, siw.real, 's:', label=r'$\Re e$')
+gs_ax[1].plot(wnfit, siw.imag, 'o:', label=r'$\Im m$')
+gs_ax[1].set_ylabel(r'$\bar{\Sigma}_{AB}$')
+gs_ax[1].legend()
 plt.xlabel(r'$\omega_n$')
-plt.title(r'$U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, BETA))
+gs_ax[0].set_title(r'$U={}$, $t_\perp={}$, $\beta={}$'.format(u_int, tp, BETA))
 plt.xlim(-5, 5)
 
 
@@ -81,7 +88,7 @@ plt.plot(wnfit, np.abs(gfit - giw.mean(0)
 plt.legend()
 plt.xlabel(r'$\omega_n$')
 plt.ylabel('error')
-plt.title(r'$U={}$, $t_\perp={}$, $\beta={}$'.format(U, tp, BETA))
+plt.title(r'$U={}$, $t_\perp={}$, $\beta={}$'.format(u_int, tp, BETA))
 plt.xlim(-5, 5)
 
 plt.show()
